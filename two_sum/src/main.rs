b90e790da8aea6ee23082e2b78a216568f172688
hinst <hinstance@yandex.ru>
2023-07-17 20:39:48 +0200

    Looping solution for Two Sum

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -8,19 +8,39 @@ fn main() {
     let output = [0, 1];
     match test(nums.to_vec(), target, output.to_vec()) {
         Err(message) => {
-            println!("[x] {message}");
-            return;
+            println!("[0] {message}");
+        }
+        _ => {}
+    }
+
+    let nums = [3, 2, 4];
+    let target = 6;
+    let output = [1, 2];
+    match test(nums.to_vec(), target, output.to_vec()) {
+        Err(message) => {
+            println!("[1] {message}");
+        }
+        _ => {}
+    }
+
+    let nums = [3, 3];
+    let target = 6;
+    let output = [0, 1];
+    match test(nums.to_vec(), target, output.to_vec()) {
+        Err(message) => {
+            println!("[2] {message}");
         }
         _ => {}
     }
 }
 
-fn test(numbers: Vec<i32>, target: i32, expectedOutput: Vec<i32>) -> Result<(), String> {
+fn test(numbers: Vec<i32>, target: i32, expected_output: Vec<i32>) -> Result<(), String> {
     let output = Solution::two_sum(numbers, target);
-    let matched = output == expectedOutput;
+    let matched = output == expected_output;
     if matched {
         return Ok(())
     } else {
-        return Err(format!("Expected: {:?}. Actual: {:?}.", expectedOutput, output))
+        let message = format!("Expected: {:?}. Actual: {:?}.", expected_output, output);
+        return Err(message)
     }
 }
--- two_sum/src/solution.rs
+++ two_sum/src/solution.rs
@@ -3,6 +3,16 @@ pub use solution::Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        return nums;
+        for first_index in 0..nums.len() {
+            let first_number = *nums.get(first_index).unwrap();
+            for second_index in first_index + 1..nums.len() {
+                let second_number = *nums.get(second_index).unwrap();
+                let sum = first_number + second_number;
+                if sum == target {
+                    return vec![first_index as i32, second_index as i32]
+                }
+            }
+        }
+        return vec![0, 0]
     }
 }
