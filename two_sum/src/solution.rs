7f3b6f7f8ea06bd39c9108ec83eca5426f4ff26b
hinst <hinstance@yandex.ru>
2023-07-17 21:22:18 +0200

    HashMap solution for Two Sum

--- two_sum/src/solution.rs
+++ two_sum/src/solution.rs
@@ -1,18 +1,19 @@
 mod solution;
 pub use solution::Solution;
 
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for first_index in 0..nums.len() {
-            let first_number = *nums.get(first_index).unwrap();
-            for second_index in first_index + 1..nums.len() {
-                let second_number = *nums.get(second_index).unwrap();
-                let sum = first_number + second_number;
-                if sum == target {
-                    return vec![first_index as i32, second_index as i32]
-                }
-            }
-        }
+        let mut numbers: HashMap<i32, usize> = HashMap::new();
+        for (index, number) in nums.iter().enumerate() {
+            let desired_number = target - number;
+            let desired_index = numbers.get(&desired_number);
+            match desired_index {
+                Some(desired_index) => return vec![*desired_index as i32, index as i32],
+                None => numbers.insert(*number, index),
+            };
+        };
         return vec![0, 0]
     }
 }
