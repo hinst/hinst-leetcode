64b5b9afec433eb849e06ee755ff49208c081142
hinst <hinstance@yandex.ru>
2023-07-17 20:19:54 +0200

    Test the solution

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,5 +1,26 @@
 mod solution;
 
+use solution::Solution;
+
 fn main() {
-    println!("Hello, world!");
+    let nums = [2, 7, 11, 15];
+    let target = 9;
+    let output = [0, 1];
+    match test(nums.to_vec(), target, output.to_vec()) {
+        Err(message) => {
+            println!("[x] {message}");
+            return;
+        }
+        _ => {}
+    }
+}
+
+fn test(numbers: Vec<i32>, target: i32, expectedOutput: Vec<i32>) -> Result<(), String> {
+    let output = Solution::two_sum(numbers, target);
+    let matched = output == expectedOutput;
+    if matched {
+        return Ok(())
+    } else {
+        return Err(format!("Expected: {:?}. Actual: {:?}.", expectedOutput, output))
+    }
 }
--- two_sum/src/solution.rs
+++ two_sum/src/solution.rs
@@ -1,8 +1,8 @@
 mod solution;
-use solution::Solution;
+pub use solution::Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-
+        return nums;
     }
 }
