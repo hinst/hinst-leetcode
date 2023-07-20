1337363cce63c3567ab4e58f48e5d4caae1dab18
hinst <hinstance@yandex.ru>
2023-07-21 01:27:17 +0200

    Converting TypeScript code into Rust code. I ran into an error (compile time)

--- 3sum/Rust/src/main.rs
+++ 3sum/Rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, ops::{Deref, DerefMut}};
 
 struct Solution {}
 
@@ -15,7 +15,7 @@ impl ThreeEntryContainer {
         }
     }
 
-    pub fn add(&self, a: i32, b: i32, c: i32) {
+    pub fn add(&mut self, a: i32, b: i32, c: i32) {
         let already_exists = self.check_exists(a, b, c) ||
             self.check_exists(a, c, b) ||
             self.check_exists(b, a, c) ||
@@ -24,17 +24,19 @@ impl ThreeEntryContainer {
             self.check_exists(c, b, a);
         if !already_exists {
             self.entries.push(vec![a, b, c]);
-            let mut first_map = self.entries_map.get(&a);
+            let mut first_map = self.entries_map.get_mut(&a);
             if None == first_map {
                 let new_map: HashMap<i32, HashSet<i32>> = HashMap::new();
-                first_map = Some(&self.entries_map.insert(a, new_map).unwrap());
+                self.entries_map.insert(a, new_map);
+                first_map = self.entries_map.get_mut(&a);
             }
-            let mut second_set = first_map.unwrap().get(&b);
+            let first_map = first_map.unwrap();
+            let mut second_set = first_map.get_mut(&b);
             if None == second_set {
                 let new_set: HashSet<i32> = HashSet::new();
-                second_set = Some(&first_map.unwrap().insert(b, new_set).unwrap());
+                first_map.insert(b, new_set);
             }
-            second_set.unwrap().insert(c);
+            second_set.as_mut().unwrap().insert(c);
         }
     }
 
@@ -54,8 +56,33 @@ impl ThreeEntryContainer {
 }
 
 impl Solution {
+    fn create_map(numbers: &Vec<i32>) -> HashMap<i32, usize> {
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        for (index, number) in numbers.iter().enumerate() {
+            map.insert(*number, index);
+        }
+        return map;
+    }
+
     pub fn three_sum(numbers: Vec<i32>) -> Vec<Vec<i32>> {
+        let map = Solution::create_map(&numbers);
         let mut results = ThreeEntryContainer::new();
+        for (first_index, first_item) in numbers.iter().enumerate() {
+            for (second_index, second_item) in numbers.iter().skip(first_index).enumerate() {
+                let desired_item = 0 - first_item - second_item;
+                let third_index = map.get(&desired_item);
+                match third_index {
+                    Some(third_index_ref) => {
+                        let third_index = *third_index_ref;
+                        if third_index != first_index && third_index != second_index {
+                            results.add(*first_item, *second_item, desired_item);
+                        }
+                    },
+                    _ => {},
+                }
+            }
+        };
+        return results.entries;
     }
 }
 
