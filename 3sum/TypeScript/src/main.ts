eff848d45c699dac07acb6acebb75c42735daf77
hinst <hinstance@yandex.ru>
2023-07-20 21:24:22 +0200

    Completed code for task Three Sum

--- 3sum/TypeScript/src/main.ts
+++ 3sum/TypeScript/src/main.ts
@@ -1,9 +1,43 @@
 type ThreeNumbers = [number, number, number];
 
-function threeSum(nums: number[]): number[][] {
+class ThreeEntryContainer {
+    private entriesMap = new Map<number, Map<number, Set<number>>>();
+    public entries: ThreeNumbers[] = [];
+
+    add(a: number, b: number, c: number) {
+        const alreadyExists = this.checkExists(a, b, c) ||
+            this.checkExists(a, c, b) ||
+            this.checkExists(b, a, c) ||
+            this.checkExists(b, c, a) ||
+            this.checkExists(c, a, b) ||
+            this.checkExists(c, b, a);
+        const canAdd = !alreadyExists;
+        if (canAdd) {
+            this.entries.push([a, b, c]);
+            let firstMap = this.entriesMap.get(a);
+            if (!firstMap) {
+                firstMap = new Map();
+                this.entriesMap.set(a, firstMap);
+            }
+            let secondSet = firstMap.get(b);
+            if (!secondSet) {
+                secondSet = new Set();
+                firstMap.set(b, secondSet);
+            }
+            secondSet.add(c);
+        }
+        return canAdd;
+    }
+
+    private checkExists(a: number, b: number, c: number): boolean | undefined {
+        return this.entriesMap.get(a)?.get(b)?.has(c);
+    }
+}
+
+function threeSum(nums: number[]): ThreeNumbers[] {
     const map = new Map<number, number>(nums.map((item, index) => [item, index]));
-    const matchedEntries: ThreeNumbers[] = [];
-    nums.forEach((firstItem, firstIndex) => {
+    const results = new ThreeEntryContainer();
+    nums.forEach(function(firstItem, firstIndex) {
         for (let secondIndex = firstIndex + 1; secondIndex < nums.length; ++secondIndex) {
             const secondItem = nums[secondIndex];
             const desiredItem = 0 - firstItem - secondItem;
@@ -11,20 +45,33 @@ function threeSum(nums: number[]): number[][] {
             const matched = thirdIndex !== undefined && secondIndex < thirdIndex &&
                 thirdIndex != firstIndex && thirdIndex != secondIndex;
             if (matched)
-                matchedEntries.push([firstItem, secondItem, desiredItem]);
+                results.add(firstItem, secondItem, desiredItem);
         }
     });
-    return matchedEntries;
-};
-
-function equals(a: any, b: any): boolean {
-    return JSON.stringify(a) === JSON.stringify(b);
+    return results.entries;
 }
 
-function test(numbers: number[], expectedOutput: number[][]) {
+function checkArraysEqual(a: any[], b: any[]) {
+    return a.length == b.length && a.every((value, index) => value == b[index]);
+}
+
+function checkEqual(a: ThreeNumbers[], b: ThreeNumbers[]): boolean {
+    a = [...a];
+    a.forEach((value, index) => {
+        a[index] = value.sort((a, b) => a - b);
+    });
+    b.forEach((value, index) => {
+        b[index] = value.sort((a, b) => a - b);
+    });
+    return a.length == b.length && a.every(aEntry => b.some(bEntry => checkArraysEqual(aEntry, bEntry)));
+}
+
+function test(numbers: number[], expectedOutput: ThreeNumbers[]) {
     const output = threeSum(numbers);
-    const matched = equals(expectedOutput, output);
-    console.log(matched, numbers, '...', expectedOutput, '...', output);
+    const matched = checkEqual(expectedOutput, output);
+    console.log(matched, numbers, 'expected', expectedOutput, 'actual', output);
 }
 
-test([-1,0,1,2,-1,-4], [[-1,-1,2],[-1,0,1]])
+test([-1,0,1,2,-1,-4], [[-1,-1,2],[-1,0,1]]);
+test([0,1,1], []);
+test([0,0,0], [[0,0,0]);
