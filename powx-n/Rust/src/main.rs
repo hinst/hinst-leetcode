691ab37e17d610cf5662076735fb2b80f3e90e92
hinst <hinstance@yandex.ru>
2023-07-27 21:06:38 +0200

    Updating readme file for task Pow(x, n)

--- powx-n/Rust/src/main.rs
+++ powx-n/Rust/src/main.rs
@@ -5,7 +5,17 @@ impl Solution {
         let mut result: f64 = 1.0;
         let mut n_abs: i64 = if n >= 0 { n as i64 } else { -(n as i64) };
         let x_squared = if n_abs >= 2 { x * x } else { 1.0 };
-        while n_abs > 2 {
+        let x_four = if n_abs >= 4 { x_squared * x_squared } else { 1.0 };
+        let x_eight = if n_abs >= 8 { x_four * x_four } else { 1.0 };
+        while n_abs >= 8 {
+            result *= x_eight;
+            n_abs -= 8;
+        }
+        while n_abs >= 4 {
+            result *= x_four;
+            n_abs -= 4;
+        }
+        while n_abs >= 2 {
             result *= x_squared;
             n_abs -= 2;
         }
