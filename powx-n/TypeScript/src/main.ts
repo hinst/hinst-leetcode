faa0c8f306e5feb02c168daeb1c830b6de2fc130
hinst <hinstance@yandex.ru>
2023-07-27 15:59:16 +0200

    Accepted solution for task 50. Pow(x, n)

--- powx-n/TypeScript/src/main.ts
+++ powx-n/TypeScript/src/main.ts
@@ -1,18 +1,31 @@
 function myPow(x: number, n: number): number {
     let result = 1;
-    if (n >= 0)
-        for (let i = 0; i < n; ++i) {
-            result *= x;
-        }
-    else {
-        for (let i = 0; i < -n; i++) {
-            result *= x;
-        }
-        result = 1 / result;
+    let nAbs = Math.abs(n);
+    const xSquared = nAbs >= 2 ? x * x : 1;
+    const xTripled = nAbs >= 3 ? xSquared * x : 1;
+    const xFour = nAbs >= 4 ? xTripled * x : 1;
+    while (nAbs > 4) {
+        result *= xFour;
+        nAbs -= 4;
     }
+    while (nAbs > 3) {
+        result *= xTripled;
+        nAbs -= 3;
+    }
+    while (nAbs > 2) {
+        result *= xSquared;
+        nAbs -= 2;
+    }
+    while (nAbs > 0) {
+        result *= x;
+        nAbs -= 1;
+    }
+    if (n < 0)
+        result = 1 / result;
     return result;
-};
+}
 
+console.log(myPow(3, 1));
 console.log(myPow(3, 2));
 console.log(myPow(2, 10));
 console.log(myPow(2, -2));
