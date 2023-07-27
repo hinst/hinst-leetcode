2c26dc3be38f06756db15dc77b57673a09289bb8
hinst <hinstance@yandex.ru>
2023-07-27 20:52:40 +0200

    Avoid i32 overflow in pow(x,n)

--- powx-n/Rust/src/main.rs
+++ powx-n/Rust/src/main.rs
@@ -3,7 +3,7 @@ struct Solution {}
 impl Solution {
     pub fn my_pow(x: f64, n: i32) -> f64 {
         let mut result: f64 = 1.0;
-        let mut n_abs = if n >= 0 { n } else { -n };
+        let mut n_abs: i64 = if n >= 0 { n as i64 } else { -(n as i64) };
         let x_squared = if n_abs >= 2 { x * x } else { 1.0 };
         while n_abs > 2 {
             result *= x_squared;
@@ -21,6 +21,6 @@ impl Solution {
 }
 
 fn main() {
-    let result = Solution::my_pow(2.0, 10);
+    let result = Solution::my_pow(2.00000, -2147483648);
     println!("{result}");
 }
