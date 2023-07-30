9329ff9aeafbd1c7c815d14ce845cbcc33d14b23
hinst <hinstance@yandex.ru>
2023-07-30 17:08:37 +0200

    Produced accepted solution for task Minimum Size Subarray Sum

--- minimum-size-subarray-sum/Rust/src/main.rs
+++ minimum-size-subarray-sum/Rust/src/main.rs
@@ -1,3 +1,45 @@
+struct Solution {}
+
+impl Solution {
+    pub fn min_sub_array_len(target: i32, numbers: Vec<i32>) -> i32 {
+        let mut begin = 0;
+        let mut end = 0;
+        let mut sum = 0;
+        let mut min_size = 0;
+        while end < numbers.len() {
+            sum += numbers.get(end).unwrap();
+            end += 1;
+            if sum >= target {
+                let size = end - begin;
+                if min_size == 0 || size <= min_size {
+                    min_size = size;
+                }
+            }
+            while sum >= target {
+                sum -= numbers.get(begin).unwrap();
+                begin += 1;
+                let size = end - begin;
+                if sum >= target && (min_size == 0 || size <= min_size) {
+                    min_size = size;
+                }
+            }
+        }
+        return min_size as i32;
+    }
+}
+
+fn test(target: i32, numbers: &[i32]) {
+    let output = Solution::min_sub_array_len(target, numbers.to_vec());
+    println!("{output}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let target = 7; let nums = [2,3,1,2,4,3];
+    test(target, &nums);
+
+    let target = 4; let nums = [1,4,4];
+    test(target, &nums);
+
+    let target = 11; let nums = [1,1,1,1,1,1,1,1];
+    test(target, &nums);
 }
