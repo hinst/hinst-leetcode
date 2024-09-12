502e8a5086aafece0790601fb1d21cde38b5f93a
hinst <hinstance@yandex.ru>
2024-09-13 00:26:48 +0200

    Changed the solution to use int32 arrays

--- zigzag-conversion/main.ts
+++ zigzag-conversion/main.ts
@@ -1,12 +1,14 @@
+const MAX_UINT32 = 4294967295;
+const MAX_UINT16 = 65535;
+
 function convert(s: string, numRows: number): string {
-    const rows: string[] = new Array(numRows).fill('');
+    const rows: Uint32Array[] = new Array(numRows).fill(undefined)
+        .map(() => new Uint32Array(s.length));
     let x = 0;
     let y = 0;
     let ySpeed = numRows > 1 ? 1 : 0;
     for (const character of s) {
-        if (rows[y].length < x)
-            rows[y] += '\0'.repeat(x - rows[y].length);
-        rows[y] += character;
+        rows[y][x] = character.charCodeAt(0);
         if (numRows > 1)
             if (y >= numRows - 1)
                 ySpeed = -1;
@@ -16,14 +18,20 @@ function convert(s: string, numRows: number): string {
         if (ySpeed < 1)
             x += 1;
     }
-    let result = '';
+    const result = new Uint32Array(s.length);
+    let resultIndex = 0;
     for (y = 0; y < rows.length; y++) {
+        rows[y][x + 1] = MAX_UINT32;
         for (const character of rows[y]) {
-            if (character != '\0')
-                result += character;
+            if (character === MAX_UINT32)
+                break;
+            if (character != 0) {
+                result[resultIndex] = character;
+                resultIndex++;
+            }
         }
     }
-    return result;
+    return String.fromCodePoint(...result);
 };
 
-console.log(convert('123456789X', 4));
+console.log(convert('123456789X', 3));
