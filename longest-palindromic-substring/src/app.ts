19f934492c8ef9876c7498e4965a211424a558e9
hinst <hinstance@yandex.ru>
2024-09-12 22:02:36 +0200

    Adding initial solution for the task

--- longest-palindromic-substring/src/app.ts
+++ longest-palindromic-substring/src/app.ts
@@ -1,5 +1,24 @@
 function longestPalindrome(s: string): string {
-
+    for (let iLength = s.length; iLength > 0; --iLength) {
+        const startLimit = s.length - iLength;
+        for (let start = 0; start <= startLimit; ++start) {
+            const end = start + iLength;
+            // console.log(s.substring(start, end), checkPalindrome(s, start, end - 1));
+            if (checkPalindrome(s, start, end - 1))
+                return s.substring(start, end);
+        }
+    }
+    return '';
 };
+
+function checkPalindrome(s: string, start: number, end: number) {
+    while (start < end) {
+        if (s[start] !== s[end])
+            return false;
+        ++start;
+        --end;
+    }
+    return true;
+}
 
 console.log(longestPalindrome("babad"));
