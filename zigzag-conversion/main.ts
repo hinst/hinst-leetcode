f9247665099a67b33770d31b1feb474c3340634b
hinst <hinstance@yandex.ru>
2024-09-13 10:14:56 +0200

    Revert "Changed the solution to use int32 arrays"
    This reverts commit 502e8a5086aafece0790601fb1d21cde38b5f93a.

--- zigzag-conversion/main.ts
+++ zigzag-conversion/main.ts
@@ -1,14 +1,12 @@
-const MAX_UINT32 = 4294967295;
-const MAX_UINT16 = 65535;
-
 function convert(s: string, numRows: number): string {
-    const rows: Uint32Array[] = new Array(numRows).fill(undefined)
-        .map(() => new Uint32Array(s.length));
+    const rows: string[] = new Array(numRows).fill('');
     let x = 0;
     let y = 0;
     let ySpeed = numRows > 1 ? 1 : 0;
     for (const character of s) {
-        rows[y][x] = character.charCodeAt(0);
+        if (rows[y].length < x)
+            rows[y] += '\0'.repeat(x - rows[y].length);
+        rows[y] += character;
         if (numRows > 1)
             if (y >= numRows - 1)
                 ySpeed = -1;
@@ -18,20 +16,14 @@ function convert(s: string, numRows: number): string {
         if (ySpeed < 1)
             x += 1;
     }
-    const result = new Uint32Array(s.length);
-    let resultIndex = 0;
+    let result = '';
     for (y = 0; y < rows.length; y++) {
-        rows[y][x + 1] = MAX_UINT32;
         for (const character of rows[y]) {
-            if (character === MAX_UINT32)
-                break;
-            if (character != 0) {
-                result[resultIndex] = character;
-                resultIndex++;
-            }
+            if (character != '\0')
+                result += character;
         }
     }
-    return String.fromCodePoint(...result);
+    return result;
 };
 
-console.log(convert('123456789X', 3));
+console.log(convert('123456789X', 4));
