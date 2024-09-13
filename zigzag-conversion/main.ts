f42d57c96b851a47abd65e9f2df1102ac0a972f5
hinst <hinstance@yandex.ru>
2024-09-13 23:22:59 +0200

    Trying to find block size

--- zigzag-conversion/main.ts
+++ zigzag-conversion/main.ts
@@ -1,11 +1,25 @@
 function convert(s: string, numRows: number): string {
     const rows: string[] = new Array(numRows).fill('');
+    const characters = new Uint16Array(s.length).fill(' '.charCodeAt(0));
     let x = 0;
     let y = 0;
     let ySpeed = numRows > 1 ? 1 : 0;
+    const blockSize = numRows + Math.max(0, numRows - 2);
+    const blockWidth = numRows - 1;
+    const blockCount = s.length / blockSize;
+    const leftoverSize = s.length % blockSize;
+    const tailSize = Math.max(0, leftoverSize - numRows);
+    console.log({ blockSize, blockCount, leftoverSize });
+    const rowWidths = new Array(numRows).fill(Math.floor(blockCount) * blockWidth);
+    for (let i = 0; i < leftoverSize - tailSize; i++)
+        rowWidths[i] += 1;
+    for (let i = 0; i < tailSize; i++)
+        rowWidths[numRows - i - 2] += i + 1;
+
+    console.log({ rowWidths });
     for (const character of s) {
         if (rows[y].length < x)
-            rows[y] += '\0'.repeat(x - rows[y].length);
+            rows[y] += ' '.repeat(x - rows[y].length);
         rows[y] += character;
         if (numRows > 1)
             if (y >= numRows - 1)
@@ -18,12 +32,14 @@ function convert(s: string, numRows: number): string {
     }
     let result = '';
     for (y = 0; y < rows.length; y++) {
+        console.log(rows[y]);
         for (const character of rows[y]) {
-            if (character != '\0')
+            if (character != ' ')
                 result += character;
         }
     }
+    console.log(String.fromCodePoint(...characters));
     return result;
 };
 
-console.log(convert('123456789X', 4));
+console.log(convert('123456789ABC', 4));
