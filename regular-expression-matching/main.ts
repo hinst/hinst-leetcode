d84a63db51b452488fe8f0013b223f27a8812fb9
hinst <hinstance@yandex.ru>
2024-09-14 21:25:32 +0200

    Optimized solution: use text index instead of substring. However, it helped only a little: time improved from 1260ms to 1160ms, beats 5%. Memory improved from 58MB to 52MB, beats 52%.

--- regular-expression-matching/main.ts
+++ regular-expression-matching/main.ts
@@ -1,20 +1,19 @@
-function isMatch(text: string, pattern: string): boolean {
-    if (text.length === 0 && pattern.length === 0)
-        return true;
-    if (pattern[0] === '.' && pattern[1] !== '*')
-        return text.length > 0 && isMatch(text.substring(1), pattern.substring(1));
-    if (pattern.length > 1 && pattern[1] === '*') {
-        const character = pattern[0];
-        const subPattern = pattern.substring(2);
+function isMatch(text: string, pattern: string, textIndex: number = 0, patternIndex: number = 0): boolean {
+    if (text.length <= textIndex && pattern.length <= patternIndex)
+        return textIndex === text.length && patternIndex === pattern.length;
+    if (pattern[patternIndex] === '.' && pattern[patternIndex + 1] !== '*')
+        return text.length > 0 && isMatch(text, pattern, textIndex + 1, patternIndex + 1);
+    if (pattern[patternIndex + 1] === '*') {
+        const character = pattern[patternIndex];
         for (let i = 0; i <= text.length; i++) {
-            if (isMatch(text.substring(i), subPattern))
+            if (isMatch(text, pattern, textIndex + i, patternIndex + 2))
                 return true;
-            if (character !== '.' && text[i] !== character)
+            if (character !== '.' && text[textIndex + i] !== character)
                 break;
         }
         return false;
     }
-    return text[0] === pattern[0] && isMatch(text.substring(1), pattern.substring(1));
+    return text[textIndex] === pattern[patternIndex] && isMatch(text, pattern, textIndex + 1, patternIndex + 1);
 };
 
-console.log(isMatch('ab', '.*'));
+console.log(isMatch('mississippi', 'mis*is*ip*.'));
