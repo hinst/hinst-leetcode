841a3df25e63af847495fd77c992f08099ac5f8b
hinst <hinstance@yandex.ru>
2024-09-20 14:46:49 +0200

    Ready solution for 24. Swap Nodes in Pairs

--- swap-nodes-in-pairs/main.ts
+++ swap-nodes-in-pairs/main.ts
@@ -1,5 +1,29 @@
 function swapPairs(head: ListNode | null): ListNode | null {
-
+    let resultHead: ListNode | null = null;
+    let resultLast: ListNode | null = null;
+    let isMajor = true;
+    while (head) {
+        const next = head.next;
+        if (isMajor) {
+            const currentNode = new ListNode(head.val);
+            if (next) {
+                const nextNode = new ListNode(next.val);
+                if (resultLast)
+                    resultLast.next = nextNode;
+                if (!resultHead)
+                    resultHead = nextNode;
+                resultLast = nextNode;
+            }
+            if (resultLast)
+                resultLast.next = currentNode;
+            if (!resultHead)
+                resultHead = currentNode;
+            resultLast = currentNode;
+        }
+        head = next;
+        isMajor = !isMajor;
+    }
+    return resultHead;
 }
 
 /** Definition for singly-linked list. */
@@ -11,3 +35,30 @@ class ListNode {
         this.next = (next===undefined ? null : next)
     }
 }
+
+function createList(numbers: number[]): ListNode | null {
+    let head: ListNode | null = null;
+    let last: ListNode | null = null;
+    for (const item of numbers) {
+        const current = new ListNode(item);
+        if (head == null) {
+            head = current;
+            last = current;
+        } else if (last != null) {
+            last.next = current;
+            last = current;
+        }
+    }
+    return head;
+}
+
+function createArray(list: ListNode | null): number[] {
+    const result: number[] = [];
+    while (list) {
+        result.push(list.val);
+        list = list.next;
+    }
+    return result;
+}
+
+console.log(createArray(swapPairs(createList([1,2,3,4,5]))));
