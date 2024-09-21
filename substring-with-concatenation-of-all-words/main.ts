799450c038576b1933e84e8051379a2cfcd359d2
hinst <hinstance@yandex.ru>
2024-09-21 15:45:28 +0200

    Updated solution, but the number of passed test cases remains the same

--- substring-with-concatenation-of-all-words/main.ts
+++ substring-with-concatenation-of-all-words/main.ts
@@ -1,27 +1,41 @@
 function findSubstring(s: string, wordArray: string[]): number[] {
-	// If an individual word has no matches, then the combined text has no chance to have a match
-	for (const word of wordArray)
-		if (s.indexOf(word) === -1)
+	const matchedIndexes = new Array<Set<number>>(...wordArray.map(word => {
+		const indexes = new Set<number>();
+			for (
+				let textIndex = s.indexOf(word);
+				textIndex !== -1;
+				textIndex = s.indexOf(word, textIndex + 1)
+			)
+				indexes.add(textIndex);
+		return indexes;
+	}));
+	for (const matchedSet of matchedIndexes)
+		if (!matchedSet.size)
 			return [];
 
 	const availableIndexes = new Set(wordArray.map((_, index) => index));
-	const currentWords: string[] = [];
+	const currentIndexes: number[] = [];
 	const results = new Set<number>();
 	function check() {
-		if (currentWords.length === wordArray.length) {
-			const text = currentWords.join('');
-			for (
-				let textIndex = s.indexOf(text);
-				textIndex !== -1;
-				textIndex = s.indexOf(text, textIndex + 1)
-			)
-				results.add(textIndex);
+		if (currentIndexes.length === wordArray.length) {
+			const startingPoints = matchedIndexes[currentIndexes[0]];
+			for (const startingPoint of startingPoints) {
+				let matched = true;
+				let sumIndex = startingPoint;
+				for (let i = 1; i < currentIndexes.length; ++i) {
+					sumIndex += wordArray[currentIndexes[0]].length;
+					if (!matchedIndexes[currentIndexes[i]].has(sumIndex))
+						matched = false;
+				}
+				if (matched)
+					results.add(startingPoint);
+			}
 		} else
 			for (const availableIndex of new Set(availableIndexes)) {
 				availableIndexes.delete(availableIndex);
-				currentWords.push(wordArray[availableIndex]);
+				currentIndexes.push(availableIndex);
 				check();
-				currentWords.pop();
+				currentIndexes.pop();
 				availableIndexes.add(availableIndex);
 			}
 	}
@@ -34,9 +48,12 @@ let words: string[];
 
 s = "foobarfoobar", words = ["foo","bar"];
 console.log(findSubstring(s, words));
+console.warn('---');
 
 s = "barfoofoobarthefoobarman", words = ["bar","foo","the"];
 console.log(findSubstring(s, words));
+console.warn('---');
 
 s = "wordgoodgoodgoodbestword"; words = ["word","good","best","good"];
 console.log(findSubstring(s, words));
+console.warn('---');
