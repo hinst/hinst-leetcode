28577e46415380400262ea03e2a24bde7508be46
hinst <hinstance@yandex.ru>
2024-10-01 02:33:07 +0200

    Adding code to handle backwards case

--- next-permutation/main.ts
+++ next-permutation/main.ts
@@ -1,15 +1,27 @@
 function nextPermutation(numbers: number[]): void {
+	let done = false;
 	for (let i = numbers.length - 2; i >= 0 ; --i) {
 		const iNext = i + 1;
 		if (numbers[i] < numbers[iNext]) {
 			const buffer = numbers[i];
 			numbers[i] = numbers[iNext];
 			numbers[iNext] = buffer;
+			done = true;
 			break;
+		}
+	}
+	if (!done) {
+		console.log('not done');
+		const limit = Math.trunc(numbers.length / 2);
+		for (let i = 0; i < limit; ++i) {
+			const iRight = numbers.length - 1 - i;
+			const buffer = numbers[i];
+			numbers[i] = numbers[iRight];
+			numbers[iRight] = buffer;
 		}
 	}
 }
 
-const nums = [1,2,3];
+const nums = [3,2,1];
 nextPermutation(nums);
 console.log(nums);
