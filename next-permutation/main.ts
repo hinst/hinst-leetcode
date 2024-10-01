f5aae367ba5ff49cb16119ebeda2258b3633e8bf
hinst <hinstance@yandex.ru>
2024-10-01 21:18:43 +0200

    Refactoring. Adding function: findSwappingIndex

--- next-permutation/main.ts
+++ next-permutation/main.ts
@@ -1,20 +1,8 @@
 function nextPermutation(numbers: number[]): void {
 	const turningIndex = findTurningIndex(numbers);
-	if (turningIndex !== -1) {
-		const turningValue = numbers[turningIndex];
-		for (let swappingIndex = numbers.length - 1; turningIndex < swappingIndex; --swappingIndex)
-			if (turningValue < numbers[swappingIndex]) {
-				swap(numbers, turningIndex, swappingIndex);
-				break;
-			}
-	}
+	if (turningIndex !== -1)
+		swap(numbers, turningIndex, findSwappingIndex(numbers, turningIndex));
 	reverseArray(numbers, turningIndex + 1);
-}
-
-function swap(numbers: number[], firstIndex: number, secondIndex: number) {
-	const buffer = numbers[firstIndex];
-	numbers[firstIndex] = numbers[secondIndex];
-	numbers[secondIndex] = buffer;
 }
 
 /** Find the first index in the array, where current value is less the next value.
@@ -24,6 +12,20 @@ function findTurningIndex(numbers: number[]) {
 		if (numbers[i] < numbers[i + 1])
 			return i;
 	return -1;
+}
+
+function findSwappingIndex(numbers: number[], turningIndex: number) {
+	const turningValue = numbers[turningIndex];
+	for (let swappingIndex = numbers.length - 1; turningIndex < swappingIndex; --swappingIndex)
+		if (turningValue < numbers[swappingIndex])
+			return swappingIndex;
+	return -1;
+}
+
+function swap(numbers: number[], firstIndex: number, secondIndex: number) {
+	const buffer = numbers[firstIndex];
+	numbers[firstIndex] = numbers[secondIndex];
+	numbers[secondIndex] = buffer;
 }
 
 function reverseArray(numbers: number[], start: number) {
