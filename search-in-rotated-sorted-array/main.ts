048b8a49776f6870ac7b7876e631c3c9540c88a3
hinst <hinstance@yandex.ru>
2024-10-02 23:44:35 +0200

    Adding diary entry for Longest Valid Parentheses. Adding task 33. Search in Rotated Sorted Array

---
+++ search-in-rotated-sorted-array/main.ts
@@ -0,0 +1,9 @@
+function search(nums: number[], target: number): number {
+    for (let i = 0; i < nums.length; ++i) {
+		if (nums[i] === target)
+			return i;
+	}
+	return -1;
+}
+
+export const searchPublic = search;
