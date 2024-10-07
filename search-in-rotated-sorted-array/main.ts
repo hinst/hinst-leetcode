7d085c18fd96e962e845a1c8842fb1e8b0c73e19
hinst <hinstance@yandex.ru>
2024-10-07 23:47:03 +0200

    Implementing solution for task 33. Search in Rotated Sorted Array using sorted search, as required in the description:
    "You must write an algorithm with O(log n) runtime complexity"

--- search-in-rotated-sorted-array/main.ts
+++ search-in-rotated-sorted-array/main.ts
@@ -1,9 +1,63 @@
-function search(nums: number[], target: number): number {
-    for (let i = 0; i < nums.length; ++i) {
-		if (nums[i] === target)
-			return i;
+function search(numbers: number[], target: number): number {
+	const offset = findTurningPosition(numbers, 0, numbers.length - 1);
+	console.log('turning point:', offset);
+	return findInShifted(numbers, 0, numbers.length - 1, offset, target);
+}
+
+function findTurningPosition(numbers: number[], left: number, right: number): number {
+	console.log(numbers.map((number, index) => (left <= index && index <= right ? number : '_')).join());
+	if (numbers[left] <= numbers[right])
+		return (right + 1) % numbers.length;
+	const middle = Math.floor((left + right) / 2);
+	if (numbers[left] < numbers[middle])
+		return findTurningPosition(numbers, middle, right);
+	else
+		return findTurningPosition(numbers, left, middle);
+}
+
+function findInShifted(numbers: number[], left: number, right: number, offset: number, target: number): number {
+	function getTrueIndex(index: number) {
+		index += offset;
+		while (index < 0)
+			index += numbers.length;
+		while (index >= numbers.length)
+			index -= numbers.length;
+		return index;
 	}
-	return -1;
+	function getNumberAt(index: number) {
+		return numbers[getTrueIndex(index)];
+	}
+	let text = '';
+	console.log(offset);
+	for (let i = 0; i < numbers.length; ++i) {
+		if (left <= i && i <= right)
+			text += getNumberAt(i) + ',';
+		else
+			text += '_,';
+	}
+	console.log(text);
+	if (left >= right || left + 1 === right)
+		return getNumberAt(left) === target
+			? left
+			: getNumberAt(right) === target
+				? right
+				: -1;
+	const middle = Math.floor((left + right) / 2);
+	if (getNumberAt(middle) < target)
+		return findInShifted(numbers, middle, right, offset, target);
+	else
+		return findInShifted(numbers, left, middle, offset, target);
 }
 
 export const searchPublic = search;
+
+
+console.log(search([6,7,8,0,1,2,3,4,5], 7));
+console.log('---');
+console.log(search([4,5,6,7,8,9,0,1,2], 0));
+console.log('---');
+console.log(search([0,1,2,3,4,5,6,7], 0));
+console.log('---');
+console.log(search([7,0,1,2,3,4,5,6], 0));
+console.log('---');
+console.log(search([1,2,3,4,5,6,7,0], 0));
--- search-in-rotated-sorted-array/main_test.ts
+++ search-in-rotated-sorted-array/main_test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "@std/assert";
-import { searchPublic as search } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { searchPublic as search } from './main.ts';
 
 Deno.test(function addTest() {
-	assertEquals(search([4,5,6,7,0,1,2], 0), 4);
+	assertEquals(search([4,5,6,7,8,9,0,1,2], 0), 6);
 });
