a362a18bd1f2b282320803ab888074597ea1e9b2
hinst <hinstance@yandex.ru>
2024-10-13 15:52:08 +0200

    Filling in code to fill valid Sudoku configuration

--- sudoku-solver/main.ts
+++ sudoku-solver/main.ts
@@ -1,30 +1,25 @@
-type CheckResult = { isComplete: boolean, isValid: boolean };
+type Point = { x: number, y: number };
+const DIGITS = '123456789';
 
-function checkSudoku(board: string[][]): CheckResult {
-	let isComplete = true;
-	let isValid = true;
+function checkSudoku(board: string[][]): boolean {
 	for (let x = 0; x < 9; ++x) {
 		const numbers = new Set<string>();
 		for (let y = 0; y < 9; y++) {
 			const item = board[x][y];
-			if (isValid && numbers.has(item))
-				isValid = false;
+			if (numbers.has(item))
+				return false;
 			if (item !== '.')
 				numbers.add(item);
-			else
-				isComplete = false;
 		}
 	}
 	for (let y = 0; y < 9; ++y) {
 		const numbers = new Set<string>();
 		for (let x = 0; x < 9; x++) {
 			const item = board[x][y];
-			if (isValid && numbers.has(item))
-				isValid = false;
+			if (numbers.has(item))
+				return false;
 			if (item !== '.')
 				numbers.add(item);
-			else
-				isComplete = false;
 		}
 	}
 	for (let overallX = 0; overallX < 3; ++overallX)
@@ -37,18 +32,49 @@ function checkSudoku(board: string[][]): CheckResult {
 			for (let x = offsetX; x < limitX; ++x)
 				for (let y = offsetY; y < limitY; ++y) {
 					const item = board[x][y];
-					if (isValid && numbers.has(item))
-						isValid = false;
+					if (numbers.has(item))
+						return false;
 					if (item !== '.')
 						numbers.add(item);
 				}
 		}
-	return {isComplete, isValid};
+	return true;
 }
 
 /** Do not return anything, modify board in-place instead. */
 function solveSudoku(board: string[][]): void {
+	const flexiblePoints: Point[] = [];
+	for (let x = 0; x < 9; ++x)
+		for (let y = 0; y < 9; ++y)
+			if (board[x][y] === '.')
+				flexiblePoints.push({x, y});
+	console.log(flexiblePoints);
+	const flexiblePointsLastIndex = flexiblePoints.length - 1;
+	function solveNext(i: number) {
+		const flexiblePoint = flexiblePoints[i];
+		console.log(i, flexiblePoint);
+		for (const digit of DIGITS) {
+			board[flexiblePoint.x][flexiblePoint.y] = digit;
+			if (checkSudoku(board)) {
+				console.log(digit);
+				printBoard(board);
+				if (i === flexiblePointsLastIndex) {
+					return;
+				} else
+					solveNext(i + 1);
+			}
+		}
+	}
+	solveNext(0);
+}
 
+function printBoard(board: string[][]) {
+	for (let y = 0; y < 9; ++y)
+		console.log(board[y].join(''));
 }
 
 export const solveSudokuExported = solveSudoku;
+
+const boardInput = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
+
+solveSudoku(boardInput);
