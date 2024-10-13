26882d3bb8daa87761df89ede9b0ea295f23738b
hinst <hinstance@yandex.ru>
2024-10-13 16:09:16 +0200

    Check faster: 70 ms -> 55 ms

--- sudoku-solver/main.ts
+++ sudoku-solver/main.ts
@@ -1,44 +1,58 @@
 type Point = { x: number, y: number };
 const DIGITS = '123456789';
 
-function checkSudoku(board: string[][]): boolean {
-	for (let x = 0; x < 9; ++x) {
-		const numbers = new Set<string>();
-		for (let y = 0; y < 9; y++) {
-			const item = board[x][y];
-			if (numbers.has(item))
-				return false;
-			if (item !== '.')
-				numbers.add(item);
-		}
+class SudokuChecker {
+	numbers: Record<string, boolean> = {};
+
+	constructor() {
+		this.clear();
 	}
-	for (let y = 0; y < 9; ++y) {
-		const numbers = new Set<string>();
-		for (let x = 0; x < 9; x++) {
-			const item = board[x][y];
-			if (numbers.has(item))
-				return false;
-			if (item !== '.')
-				numbers.add(item);
-		}
+
+	clear() {
+		for (const digit of DIGITS)
+			this.numbers[digit] = false;
 	}
-	for (let overallX = 0; overallX < 3; ++overallX)
-		for (let overallY = 0; overallY < 3; ++overallY) {
-			const numbers = new Set<string>();
-			const offsetX = overallX * 3;
-			const limitX = offsetX + 3;
-			const offsetY = overallY * 3;
-			const limitY = offsetY + 3;
-			for (let x = offsetX; x < limitX; ++x)
-				for (let y = offsetY; y < limitY; ++y) {
-					const item = board[x][y];
-					if (numbers.has(item))
-						return false;
-					if (item !== '.')
-						numbers.add(item);
-				}
+
+	check(board: string[][]): boolean {
+		for (let x = 0; x < 9; ++x) {
+			this.clear();
+			for (let y = 0; y < 9; y++) {
+				const item = board[x][y];
+				if (this.numbers[item])
+					return false;
+				if (item !== '.')
+					this.numbers[item] = true;
+			}
 		}
-	return true;
+		for (let y = 0; y < 9; ++y) {
+			this.clear();
+			for (let x = 0; x < 9; x++) {
+				const item = board[x][y];
+				if (this.numbers[item])
+					return false;
+				if (item !== '.')
+					this.numbers[item] = true;
+			}
+		}
+		for (let overallX = 0; overallX < 3; ++overallX)
+			for (let overallY = 0; overallY < 3; ++overallY) {
+				this.clear();
+				const offsetX = overallX * 3;
+				const limitX = offsetX + 3;
+				const offsetY = overallY * 3;
+				const limitY = offsetY + 3;
+				for (let x = offsetX; x < limitX; ++x)
+					for (let y = offsetY; y < limitY; ++y) {
+						const item = board[x][y];
+						if (this.numbers[item])
+							return false;
+						if (item !== '.')
+							this.numbers[item] = true;
+					}
+			}
+		return true;
+	}
+
 }
 
 /** Do not return anything, modify board in-place instead. */
@@ -49,11 +63,12 @@ function solveSudoku(board: string[][]): void {
 			if (board[x][y] === '.')
 				flexiblePoints.push({x, y});
 	const flexiblePointsLastIndex = flexiblePoints.length - 1;
+	const checker = new SudokuChecker();
 	function solveNext(i: number) {
 		const flexiblePoint = flexiblePoints[i];
 		for (const digit of DIGITS) {
 			board[flexiblePoint.x][flexiblePoint.y] = digit;
-			if (checkSudoku(board)) {
+			if (checker.check(board)) {
 				if (i === flexiblePointsLastIndex)
 					return true;
 				else if (solveNext(i + 1))
@@ -75,5 +90,7 @@ export const solveSudokuExported = solveSudoku;
 
 const boardInput = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
 
+console.time('complete');
 solveSudoku(boardInput);
+console.timeEnd('complete');
 printBoard(boardInput);
