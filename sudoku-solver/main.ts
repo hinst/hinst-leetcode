de2504094a300897473ddde3d1d55da8d8ce8b85
hinst <hinstance@yandex.ru>
2024-10-13 16:37:15 +0200

    Adding optimization: skipPoint

--- sudoku-solver/main.ts
+++ sudoku-solver/main.ts
@@ -19,8 +19,10 @@ class SudokuSolver {
 			cache[digit] = false;
 	}
 
-	private check(): boolean {
+	private check(skipPoint: Point): boolean {
 		for (let x = 0; x < 9; ++x) {
+			if (x === skipPoint.x)
+				continue;
 			this.clear(this.existingNumbersCache);
 			for (let y = 0; y < 9; y++) {
 				const item = this.board[x][y];
@@ -31,6 +33,8 @@ class SudokuSolver {
 			}
 		}
 		for (let y = 0; y < 9; ++y) {
+			if (y === skipPoint.y)
+				continue;
 			this.clear(this.existingNumbersCache);
 			for (let x = 0; x < 9; x++) {
 				const item = this.board[x][y];
@@ -64,9 +68,14 @@ class SudokuSolver {
 			availableNumbers.delete(this.board[x][flexiblePoint.y]);
 		for (let y = 0; y < 9; ++y)
 			availableNumbers.delete(this.board[flexiblePoint.x][y]);
+		const offsetX = (Math.trunc(flexiblePoint.x / 3)) * 3;
+		const offsetY = (Math.trunc(flexiblePoint.y / 3)) * 3;
+		for (let x = offsetX; x < offsetX + 3; ++x)
+			for (let y = offsetY; y < offsetY + 3; ++y)
+				availableNumbers.delete(this.board[x][y]);
 		for (const digit of availableNumbers) {
 			this.board[flexiblePoint.x][flexiblePoint.y] = digit;
-			if (this.check()) {
+			if (this.check(flexiblePoint)) {
 				if (i === this.flexiblePointsLastIndex || this.solveNext(i + 1))
 					return true;
 			}
@@ -90,6 +99,7 @@ export const solveSudokuExported = solveSudoku;
 
 const boardInput = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
 
+printBoard(boardInput);
 console.time('complete');
 solveSudoku(boardInput);
 console.timeEnd('complete');
