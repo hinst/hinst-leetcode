435e9115d60e976262513f894c40831ad06e555e
hinst <hinstance@yandex.ru>
2024-10-27 22:43:56 +0100

    Preparing code for task 46. Permutations

--- permutations/main.ts
+++ permutations/main.ts
@@ -1,6 +1,9 @@
 function permute(nums: number[]): number[][] {
-
+	const results: number[][] = [];
+	return results;
 }
+
+export const permuteExported = permute;
 
 if (import.meta.main) {
 	console.log(permute([1,2,3]));
