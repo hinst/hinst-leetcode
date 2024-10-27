2b476b0fb3f7bfb713320d62e0aa5b0481b2a00e
hinst <hinstance@yandex.ru>
2024-10-27 15:05:41 +0100

    Refactoring -> class Matcher

--- wildcard-matching/main.ts
+++ wildcard-matching/main.ts
@@ -1,22 +1,62 @@
-const CACHE = new Map<number, boolean>();
+class Matcher {
+	readonly cache = new Map<number, boolean>();
+
+	constructor(private s: string, private pattern: string) {
+		this.pattern = optimizePattern(this.pattern);
+	}
+
+	isMatch(): boolean {
+		const trimResult = trimPattern(this.s, this.pattern);
+		if (!trimResult.possible)
+			return false;
+		this.s = trimResult.s;
+		this.pattern = trimResult.pattern;
+
+		let letterCount = 0;
+		for (const character of this.pattern)
+			if (character !== '*')
+				++letterCount;
+
+		return this.checkCached(0, 0, letterCount);
+	}
+
+	private checkCached(sIndex: number, patternIndex: number, remainingLetterCount: number): boolean {
+		const cacheKey = sIndex + patternIndex * 2048;
+		const cachedResult = this.cache.get(cacheKey);
+		if (cachedResult !== undefined)
+			return cachedResult;
+		const result = this.check(sIndex, patternIndex, remainingLetterCount);
+		this.cache.set(cacheKey, result);
+		return result;
+	}
+
+	private check(sIndex: number, patternIndex: number, remainingLetterCount: number): boolean {
+		if (sIndex === this.s.length && patternIndex === this.pattern.length)
+			return true;
+		if (this.pattern[patternIndex] === '?')
+			return this.s[sIndex] != null &&
+				this.checkCached(sIndex + 1, patternIndex + 1, remainingLetterCount - 1);
+		if (this.pattern[patternIndex] === '*') {
+			const nextPatternIndex = patternIndex + 1;
+			const limit = this.s.length - remainingLetterCount;
+			if (patternIndex === this.pattern.length - 1)
+				return true;
+			else if (this.pattern[nextPatternIndex] !== '?') {
+				sIndex = this.s.indexOf(this.pattern[nextPatternIndex], sIndex);
+				if (sIndex === -1)
+					return false;
+			}
+			for (let i = sIndex; i <= limit; ++i)
+				if (this.checkCached(i, nextPatternIndex, remainingLetterCount))
+					return true;
+		}
+		return this.s[sIndex] === this.pattern[patternIndex] &&
+			this.checkCached(sIndex + 1, patternIndex + 1, remainingLetterCount - 1);
+	}
+}
 
 function isMatch(s: string, pattern: string): boolean {
-	CACHE.clear();
-
-	pattern = optimizePattern(pattern);
-
-	const trimResult = trimPattern(s, pattern);
-	if (!trimResult.possible)
-		return false;
-	s = trimResult.s;
-	pattern = trimResult.pattern;
-
-	let letterCount = 0;
-	for (const character of pattern)
-		if (character !== '*')
-			++letterCount;
-
-	return checkCached(s, 0, pattern, 0, letterCount);
+	return new Matcher(s, pattern).isMatch();
 }
 
 function optimizePattern(pattern: string): string {
@@ -46,39 +86,6 @@ function trimPattern(s: string, pattern: string): {s: string, pattern: string, p
 	pattern = pattern.slice(0, iPattern + 1);
 	s = s.slice(0, iString + 1);
 	return {s, pattern, possible};
-}
-
-function checkCached(s: string, sIndex: number, pattern: string, patternIndex: number, remainingLetterCount: number): boolean {
-	const cacheKey = sIndex + patternIndex * 10_000;
-	const cachedResult = CACHE.get(cacheKey);
-	if (cachedResult !== undefined)
-		return cachedResult;
-	const result = check(s, sIndex, pattern, patternIndex, remainingLetterCount);
-	CACHE.set(cacheKey, result);
-	return result;
-}
-
-function check(s: string, sIndex: number, pattern: string, patternIndex: number, remainingLetterCount: number): boolean {
-	if (sIndex === s.length && patternIndex === pattern.length)
-		return true;
-	if (pattern[patternIndex] === '?')
-		return s[sIndex] != null && checkCached(s, sIndex + 1, pattern, patternIndex + 1, remainingLetterCount - 1);
-	if (pattern[patternIndex] === '*') {
-		const nextPatternIndex = patternIndex + 1;
-		const limit = s.length - remainingLetterCount;
-		if (patternIndex === pattern.length - 1)
-			return true;
-		else if (pattern[nextPatternIndex] !== '?') {
-			sIndex = s.indexOf(pattern[nextPatternIndex], sIndex);
-			if (sIndex === -1)
-				return false;
-		}
-		for (let i = sIndex; i <= limit; ++i)
-			if (checkCached(s, i, pattern, nextPatternIndex, remainingLetterCount))
-				return true;
-	}
-	return s[sIndex] === pattern[patternIndex] &&
-		checkCached(s, sIndex + 1, pattern, patternIndex + 1, remainingLetterCount - 1);
 }
 
 export const isMatchExported = isMatch;
