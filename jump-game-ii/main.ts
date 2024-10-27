5493741b88923d9dc07a3870c0660921214c8dc1
hinst <hinstance@yandex.ru>
2024-10-27 15:53:16 +0100

    Refactoring: using class JumpGame, initializing min == -1 instead of Number.MAX_SAFE_INTEGER

--- jump-game-ii/main.ts
+++ jump-game-ii/main.ts
@@ -1,31 +1,39 @@
-const cache = new Map<number, number>();
+class JumpGame {
+	private readonly cache = new Map<number, number>();
 
-function jumpCached(numbers: number[], index = 0): number {
-	let result = cache.get(index);
-	if (result !== undefined)
-		return result;
-	result = jumpAt(numbers, index);
-	cache.set(index, result);
-	return result;
-}
-
-function jumpAt(numbers: number[], index = 0): number {
-	if (index === numbers.length - 1)
-		return 0;
-	let min = Number.MAX_SAFE_INTEGER;
-	for (let i = numbers[index]; i > 0 ; --i) {
-		const count = jumpCached(numbers, index + i);
-		if (count < min)
-			min = count;
-		if (min === 0)
-			break;
+	constructor(private readonly numbers: number[]) {
 	}
-	return 1 + min;
+
+	private jumpCached(index: number): number {
+		let result = this.cache.get(index);
+		if (result !== undefined)
+			return result;
+		result = this.jumpAt(index);
+		this.cache.set(index, result);
+		return result;
+	}
+
+	jumpAt(index: number): number {
+		if (index === this.numbers.length - 1)
+			return 0;
+		if (index >= this.numbers.length)
+			return -1;
+		let min = -1;
+		for (let i = this.numbers[index]; i > 0 ; --i) {
+			const count = this.jumpCached(index + i);
+			if (count !== -1) {
+				if (min === -1 || count < min)
+					min = count;
+				if (min === 0)
+					break;
+			}
+		}
+		return min === -1 ? -1 : min + 1;
+	}
 }
 
 function jump(numbers: number[]): number {
-	cache.clear();
-	return jumpCached(numbers, 0);
+	return new JumpGame(numbers).jumpAt(0);
 }
 
 export const jumpExported = jump;
