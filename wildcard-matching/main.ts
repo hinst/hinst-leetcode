691ea18058ace63702dc8febe1aec32458ae835b
hinst <hinstance@yandex.ru>
2024-10-27 13:25:26 +0100

    Filling in code for wildcard matcher

--- wildcard-matching/main.ts
+++ wildcard-matching/main.ts
@@ -1,5 +1,20 @@
 function isMatch(s: string, pattern: string): boolean {
+	return check(s, 0, pattern, 0);
+}
 
+function check(s: string, sIndex: number, pattern: string, patternIndex: number): boolean {
+	if (sIndex === s.length && patternIndex === pattern.length)
+		return true;
+	if (pattern[patternIndex] === '?')
+		return s[sIndex] != null && check(s, sIndex + 1, pattern, patternIndex + 1);
+	if (pattern[patternIndex] === '*') {
+		const nextPatternIndex = patternIndex + 1;
+		for (let i = sIndex; i <= s.length; ++i)
+			if (check(s, i, pattern, nextPatternIndex))
+				return true;
+	}
+	return s[sIndex] === pattern[patternIndex] &&
+		check(s, sIndex + 1, pattern, patternIndex + 1);
 }
 
 export const isMatchExported = isMatch;
@@ -7,4 +22,5 @@ export const isMatchExported = isMatch;
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
 	console.log(isMatch('aa', 'a'));
+	console.log(isMatch('a', 'a'));
 }
