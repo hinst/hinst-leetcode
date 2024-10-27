a282362af7b46031e481e1be4d7a2690a508dd8a
hinst <hinstance@yandex.ru>
2024-10-27 15:42:35 +0100

    Adding cache -> all online test cases are now passed

--- jump-game-ii/main.ts
+++ jump-game-ii/main.ts
@@ -1,15 +1,31 @@
-function jump(numbers: number[], index = 0, depth = 0): number {
+const cache = new Map<number, number>();
+
+function jumpCached(numbers: number[], index = 0): number {
+	let result = cache.get(index);
+	if (result !== undefined)
+		return result;
+	result = jumpAt(numbers, index);
+	cache.set(index, result);
+	return result;
+}
+
+function jumpAt(numbers: number[], index = 0): number {
 	if (index === numbers.length - 1)
 		return 0;
 	let min = Number.MAX_SAFE_INTEGER;
 	for (let i = numbers[index]; i > 0 ; --i) {
-		const count = jump(numbers, index + i, depth + 1);
+		const count = jumpCached(numbers, index + i);
 		if (count < min)
 			min = count;
 		if (min === 0)
 			break;
 	}
 	return 1 + min;
+}
+
+function jump(numbers: number[]): number {
+	cache.clear();
+	return jumpCached(numbers, 0);
 }
 
 export const jumpExported = jump;
