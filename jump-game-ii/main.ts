981e287b37840b27a36822f5cee309ad325d4fd8
hinst <hinstance@yandex.ru>
2024-10-27 15:37:11 +0100

    Adding diary entry for task 45. Jump Game II

--- jump-game-ii/main.ts
+++ jump-game-ii/main.ts
@@ -1,11 +1,8 @@
 function jump(numbers: number[], index = 0, depth = 0): number {
-	// console.log({depth, index});
-	// console.log(numbers.join(''));
-	// console.log(' '.repeat(index) + '^');
 	if (index === numbers.length - 1)
 		return 0;
 	let min = Number.MAX_SAFE_INTEGER;
-	for (let i = 1; i <= numbers[index]; ++i) {
+	for (let i = numbers[index]; i > 0 ; --i) {
 		const count = jump(numbers, index + i, depth + 1);
 		if (count < min)
 			min = count;
