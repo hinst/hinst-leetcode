e69510bec3b29cda3a9c18e691ec322816e26920
hinst <hinstance@yandex.ru>
2024-10-29 10:14:26 +0100

    Completing task 46. Permutations

--- permutations/main.ts
+++ permutations/main.ts
@@ -1,6 +1,20 @@
-function permute(nums: number[]): number[][] {
-	const results: number[][] = [];
+function permute(numbers: number[], index = 0, results: number[][] = []): number[][] {
+	if (index === numbers.length)
+		results.push(numbers.slice());
+	for (let i = index; i < numbers.length; ++i) {
+		swap(numbers, index, i);
+		permute(numbers, index + 1, results);
+		swap(numbers, index, i);
+	}
 	return results;
+}
+
+function swap(numbers: number[], a: number, b: number) {
+	if (a === b)
+		return;
+	const buffer = numbers[a];
+	numbers[a] = numbers[b];
+	numbers[b] = buffer;
 }
 
 export const permuteExported = permute;
--- permutations/main_test.ts
+++ permutations/main_test.ts
@@ -1,6 +1,5 @@
 import { assertEquals } from '@std/assert';
 import { permuteExported } from './main.ts';
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function permute() {
 });
