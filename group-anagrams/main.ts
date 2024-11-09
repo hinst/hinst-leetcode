387bdba98387759698948f93711406878b390ba1
hinst <hinstance@yandex.ru>
2024-11-09 17:57:40 +0100

    Adding task 49. Group Anagrams

---
+++ group-anagrams/main.ts
@@ -0,0 +1,9 @@
+function groupAnagrams(strings: string[]): string[][] {
+	const results: string[][] = [];
+	return results;
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+	console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
+}
