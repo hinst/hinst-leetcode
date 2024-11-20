e27d771b35948642a419dc6ae1ce45f80bdde5da
hinst <hinstance@yandex.ru>
2024-11-20 20:55:32 +0100

    Implementing Kadane algorithm

--- maximum-subarray/main.ts
+++ maximum-subarray/main.ts
@@ -1,12 +1,11 @@
 function maxSubArray(numbers: number[]): number {
     let max = Number.MIN_SAFE_INTEGER;
-	for (let i = 0; i < numbers.length; ++i) {
-		let sum = 0;
-		for (let j = i; j < numbers.length; ++j) {
-			sum += numbers[j];
-			if (max < sum)
-				max = sum;
-		}
+	let previous = 0;
+	for (const item of numbers) {
+		const sum = Math.max(previous + item, item);
+		if (max < sum)
+			max = sum;
+		previous = sum;
 	}
 	return max;
 }
