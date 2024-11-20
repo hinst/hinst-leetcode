19dc7f2cf8b7df96d54302ab878a1ed2893d536f
hinst <hinstance@yandex.ru>
2024-11-20 20:14:15 +0100

    Adding unit tests

--- maximum-subarray/main_test.ts
+++ maximum-subarray/main_test.ts
@@ -1,6 +1,9 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { maxSubArrayEx } from './main.ts';
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function maxSubArray() {
+	assertEquals(maxSubArrayEx([-2,1,-3,4,-1,2,1,-5,4]), 6);
+});
+Deno.test(function maxSubArray() {
+	assertEquals(maxSubArrayEx([1]), 1);
 });
