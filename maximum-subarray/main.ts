540a3af4dff7bf3f1e4707eb477982ff767a6c97
hinst <hinstance@yandex.ru>
2024-11-20 20:06:04 +0100

    Passing up to 200 of 210 test cases

--- maximum-subarray/main.ts
+++ maximum-subarray/main.ts
@@ -1,17 +1,18 @@
 function maxSubArray(numbers: number[]): number {
     let max = Number.MIN_SAFE_INTEGER;
 	for (let i = 0; i < numbers.length; ++i) {
-		for (let j = i + 1; j < numbers.length; ++j) {
+		for (let j = i; j < numbers.length; ++j) {
 			let sum = 0;
-			for (let itemIndex = i; itemIndex < j; ++itemIndex)
+			for (let itemIndex = i; itemIndex <= j; ++itemIndex)
 				sum += numbers[itemIndex];
 			if (max < sum)
 				max = sum;
 		}
 	}
 	return max;
-};
+}
 
 if (import.meta.main) {
 	console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+	console.log(maxSubArray([1]));
 }
