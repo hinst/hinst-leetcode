70d6366fd36929788627d5fe0fdde5f7f314c303
hinst <hinstance@yandex.ru>
2024-11-22 23:28:17 +0100

    Adding cache. Attempt longest jump first. Passed all test cases

--- jump-game/main.ts
+++ jump-game/main.ts
@@ -1,10 +1,19 @@
 function canJump(numbers: number[]): boolean {
+	const cache = new Map<number, boolean>();
+	function jumpCached(index: number) {
+		const cached = cache.get(index);
+		if (cached !== undefined)
+			return cached;
+		const result = jump(index);
+		cache.set(index, result);
+		return result;
+	}
 	function jump(index: number): boolean {
 		if (index >= numbers.length - 1)
 			return true;
 		const limit = numbers[index];
-		for (let i = 1; i <= limit; ++i)
-			if (jump(index + i))
+		for (let i = limit; 0 < i; --i)
+			if (jumpCached(index + i))
 				return true;
 		return false;
 	}
