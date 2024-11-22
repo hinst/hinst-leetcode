77c76ab35c9f297ee41d2eccb8436f28118358d8
hinst <hinstance@yandex.ru>
2024-11-23 00:53:44 +0100

    Advancing to test case 136 of 171

--- merge-intervals/main.ts
+++ merge-intervals/main.ts
@@ -5,8 +5,14 @@ function collapse(target: number[], source: number[]) {
 		target[1] = source[1];
 }
 
+function sortIntervals(intervals: number[][]) {
+	intervals.sort((a, b) => (b[1] - b[0]) - (a[1] - a[0]));
+	return intervals;
+}
+
 function merge(intervals: number[][]): number[][] {
-	const mergedIntervals: boolean[] = new Array(intervals.length).fill(false);
+	sortIntervals(intervals);
+	const isMergedIntervals: boolean[] = new Array(intervals.length).fill(false);
 	const map = new Map<number, number>();
 	for (let intervalIndex = 0; intervalIndex < intervals.length; ++intervalIndex) {
 		const interval = intervals[intervalIndex];
@@ -16,7 +22,7 @@ function merge(intervals: number[][]): number[][] {
 		for (let i = left; i <= right; ++i) {
 			existing = map.get(i);
 			if (existing !== undefined) {
-				mergedIntervals[intervalIndex] = true;
+				isMergedIntervals[intervalIndex] = true;
 				collapse(intervals[existing], interval);
 			}
 		}
@@ -24,11 +30,15 @@ function merge(intervals: number[][]): number[][] {
 		for (let i = left; i <= right; ++i)
 			map.set(i, finalIndex);
 	}
-	return intervals.filter((_, index) => !mergedIntervals[index]);
+	let mergedIntervals = intervals.filter((_, index) => !isMergedIntervals[index]);
+	if (mergedIntervals.length != intervals.length)
+		mergedIntervals = merge(mergedIntervals);
+	return mergedIntervals;
 }
 
 export const mergeEx = merge;
+export const sortIntervalsEx = sortIntervals;
 
 if (import.meta.main) {
-	console.log(merge([[4,5],[1,4],[0,1]]));
+	console.log(merge([[2,3],[4,5],[6,7],[8,9],[1,10]]));
 }
--- merge-intervals/main_test.ts
+++ merge-intervals/main_test.ts
@@ -1,12 +1,27 @@
 import { assertEquals } from '@std/assert';
-import { mergeEx } from './main.ts';
+import { mergeEx, sortIntervalsEx } from './main.ts';
 
 Deno.test(function merge() {
-	assertEquals(mergeEx([[1,3],[2,6],[8,10],[15,18]]), [[1,6],[8,10],[15,18]]);
+	assertEquals(
+		sortIntervalsEx(mergeEx([[1,3],[2,6],[8,10],[15,18]])),
+		sortIntervalsEx([[1,6],[8,10],[15,18]])
+	);
 });
 Deno.test(function merge() {
-	assertEquals(mergeEx([[1,4],[4,5]]), [[1,5]]);
+	assertEquals(
+		sortIntervalsEx(mergeEx([[1,4],[4,5]])),
+		sortIntervalsEx([[1,5]])
+	);
 });
 Deno.test(function merge() {
-	assertEquals(mergeEx([[4,5],[1,4],[0,1]]), [[0,5]]);
+	assertEquals(
+		sortIntervalsEx(mergeEx([[4,5],[1,4],[0,1]])),
+		sortIntervalsEx([[0,5]])
+	);
+});
+Deno.test(function merge() {
+	assertEquals(
+		sortIntervalsEx(mergeEx([[2,3],[4,6],[5,7],[3,4]])),
+		sortIntervalsEx([[2,7]])
+	);
 });
