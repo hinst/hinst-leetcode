a67cd8fb08950db2a9b54320a9b4712bfdab8271
hinst <hinstance@yandex.ru>
2024-11-22 23:24:53 +0100

    Filling in code for task 55. Jump Game. Getting time limit exceeded on test case 76 of 173.

--- jump-game/main.ts
+++ jump-game/main.ts
@@ -1,6 +1,17 @@
 function canJump(numbers: number[]): boolean {
-	return false;
+	function jump(index: number): boolean {
+		if (index >= numbers.length - 1)
+			return true;
+		const limit = numbers[index];
+		for (let i = 1; i <= limit; ++i)
+			if (jump(index + i))
+				return true;
+		return false;
+	}
+	return jump(0);
 }
+
+export const canJumpEx = canJump;
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
--- jump-game/main_test.ts
+++ jump-game/main_test.ts
@@ -1,6 +1,9 @@
 import { assertEquals } from '@std/assert';
-import { add } from './main.ts';
+import { canJumpEx } from './main.ts';
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function canJump() {
+	assertEquals(canJumpEx([2,3,1,1,4]), true);
+});
+Deno.test(function canJump() {
+	assertEquals(canJumpEx([3,2,1,0,4]), false);
 });
