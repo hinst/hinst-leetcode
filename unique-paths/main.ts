cf85ac046bbc04ef3b5094f9d01d8d2cd21384ba
hinst <hinstance@yandex.ru>
2024-12-08 00:20:09 +0100

    Adding function subFactorial

--- unique-paths/main.ts
+++ unique-paths/main.ts
@@ -1,12 +1,20 @@
 function uniquePaths(m: number, n: number): number {
 	if (m < n)
 		[m, n] = [n, m];
-	return factorial(m + n - 2) / factorial(m - 1) / factorial(n - 1);
+	return subFactorial(m + n - 2, m - 1) / factorial(n - 1);
 }
 
 function factorial(n: number) {
 	let result = 1;
 	for (let i = 1; i <= n; ++i)
+		result *= i;
+	return result;
+}
+
+/** @returns (a! / b!) */
+function subFactorial(a: number, b: number) {
+	let result = 1;
+	for (let i = b + 1; i <= a; ++i)
 		result *= i;
 	return result;
 }
