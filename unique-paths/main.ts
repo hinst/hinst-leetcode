48e2e2aee3b934547adda3625c6ba27fa9cd2359
hinst <hinstance@yandex.ru>
2024-12-08 00:17:50 +0100

    Correcting factorial logic. Passed online test cases up to 38 of 63

--- unique-paths/main.ts
+++ unique-paths/main.ts
@@ -1,18 +1,16 @@
 function uniquePaths(m: number, n: number): number {
 	if (m < n)
 		[m, n] = [n, m];
-	return factorial(m + n - 1) / factorial(m) / factorial(n);
+	return factorial(m + n - 2) / factorial(m - 1) / factorial(n - 1);
 }
 
 function factorial(n: number) {
 	let result = 1;
-	for (let i = 1; i < n; ++i) {
+	for (let i = 1; i <= n; ++i)
 		result *= i;
-	}
 	return result;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
 	console.log(uniquePaths(3, 7));
 }
