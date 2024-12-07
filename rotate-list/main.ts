28176604e40b88c11d07a719178aa7306efbafbf
hinst <hinstance@yandex.ru>
2024-12-07 23:22:48 +0100

    Completing task 61. Rotate List

--- rotate-list/main.ts
+++ rotate-list/main.ts
@@ -1,9 +1,37 @@
 import { ListNode } from './ListNode.ts';
 
-function rotateRight(head: ListNode | null, k: number): ListNode | null {
-	if (!head)
-		return null;
+class DualListNode extends ListNode {
+	previous?: ListNode;
+}
+
+function rotateRight(head: DualListNode | null, k: number): ListNode | null {
+	if (!head || 0 === k)
+		return head;
+	let [tail, length] = fillPreviousNode(head);
+	k = k % length;
+	for (let i = 0; i < k; ++i) {
+		const oldHead = head;
+		head = tail;
+		head.next = oldHead;
+		tail = tail.previous!;
+		head.previous = undefined;
+		tail.next = null;
+	}
 	return head;
+}
+
+/** @returns [tail, length] */
+function fillPreviousNode(head: DualListNode): [DualListNode, number] {
+	let length = 0;
+	while (true) {
+		const current = head;
+		++length;
+		if (head.next) {
+			head = head.next;
+			head.previous = current;
+		} else
+			return [head, length];
+	}
 }
 
 function createList(numbers: number[]): ListNode | null {
@@ -29,5 +57,7 @@ function createArray(head: ListNode | null): number[] {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-	console.log(createArray(rotateRight(createList([1,2,3,4,5]), 4)));
+	console.log(createArray(rotateRight(createList([1,2,3,4,5]), 2)));
+	console.log(createArray(rotateRight(createList([0,1,2]), 4)));
+	console.log(createArray(rotateRight(createList([1]), 0)));
 }
