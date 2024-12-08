16d0ff51400eb6f707d616209a38dfcc4180ddfe
hinst <hinstance@yandex.ru>
2024-12-08 13:33:47 +0100

    Discarding flat & unpack

--- unique-paths-ii/main.ts
+++ unique-paths-ii/main.ts
@@ -3,17 +3,6 @@ class Point {
 		public readonly x: number,
 		public readonly y: number,
 	) {}
-
-	getFlat(width: number) {
-		return this.x + this.y * width;
-	}
-
-	static unpack(flat: number, width: number): Point {
-		return new Point(
-			flat % width,
-			Math.trunc(flat / width),
-		);
-	}
 }
 
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
@@ -22,7 +11,7 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 	function step(point: Point): boolean {
 		if (width <= point.x || height <= point.y)
 			return false;
-		if (obstacleGrid[point.y][point.x] === -1)
+		if (obstacleGrid[point.y][point.x] !== 0)
 			return false;
 		const leftX = point.x - 1;
 		const topY = point.y - 1;
@@ -37,17 +26,17 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 				obstacleGrid[y][x] = -1;
 	if (obstacleGrid[0][0] !== -1)
 		obstacleGrid[0][0] = 1;
-	const points: Set<number> = new Set([0]);
-	while (points.size) {
-		const previousPoints = Array.from(points).map(flat => Point.unpack(flat, width));
-		points.clear();
+	const points: Point[] = [new Point(0, 0)];
+	while (points.length) {
+		const previousPoints = points.slice();
+		points.length = 0;
 		for (const point of previousPoints) {
 			const right = new Point(point.x + 1, point.y);
 			const bottom = new Point(point.x, point.y + 1);
 			if (step(right))
-				points.add(right.getFlat(width));
+				points.push(right);
 			if (step(bottom))
-				points.add(bottom.getFlat(width));
+				points.push(bottom);
 		}
 	}
 	const exitCell = obstacleGrid[height - 1][width - 1];
