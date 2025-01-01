bfed905b64b15906de3cbdf433bbbe992bde2fa9
hinst <hinstance@yandex.ru>
2025-01-01 20:53:58 +0100

    Adding task 84. Largest Rectangle in Histogram

---
+++ largest-rectangle-in-histogram/main.ts
@@ -0,0 +1,21 @@
+function largestRectangleArea(heights: number[]): number {
+	let largestArea = 0;
+	for (let i = 0; i < heights.length; ++i) {
+		const height = heights[i];
+		let width = 1;
+		for (let w = i + 1; w < heights.length; ++w)
+			if (height <= heights[w])
+				++width;
+			else
+				break;
+		const area = height * width;
+		if (largestArea < area)
+			largestArea = area;
+	}
+	return largestArea;
+}
+
+if (import.meta.main) {
+	const heights = [2,1,5,6,2,3];
+	console.log(largestRectangleArea(heights));
+}
