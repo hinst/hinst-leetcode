b0ccab1bb46548e13edc56453fd33217d0eedd9b
hinst <hinstance@yandex.ru>
2025-01-05 00:45:22 +0100

    Updating comparison approach. Passed online test cases up to 180 / 290

--- scramble-string/main.ts
+++ scramble-string/main.ts
@@ -2,54 +2,37 @@ function isScramble(s1: string, s2: string): boolean {
 	const sequence = new Uint8Array(s1.length);
 	for (let i = 0; i < sequence.length; ++i)
 		sequence[i] = i;
-	const desiredSequence = findSequence(s1, s2);
-	if (!desiredSequence)
-		return false;
-	const scrambler = new Scrambler(sequence, desiredSequence);
+	const scrambler = new Scrambler(
+		sequence,
+		convertStringToArray(s1),
+		convertStringToArray(s2)
+	);
 	return scrambler.run();
 }
 
-function findSequence(s1: string, s2: string): Uint8Array | undefined {
-	if (s1.length !== s2.length)
-		return;
-	const availableCharacters = new Map<string, number[]>();
-
-	let characterIndex = 0;
-	for (const character of s1) {
-		const positions = availableCharacters.get(character) || [];
-		positions.push(characterIndex);
-		availableCharacters.set(character, positions);
-		++characterIndex;
-	}
-
-	characterIndex = 0;
-	const sequence = new Uint8Array(s1.length);
-	for (const character of s2) {
-		const positions = availableCharacters.get(character);
-		if (!positions?.length)
-			return;
-		const position = positions.shift();
-		sequence[characterIndex] = position ?? -1;
-		++characterIndex;
-	}
-	return sequence;
+function convertStringToArray(s: string): Uint8Array {
+	const result: number[] = [];
+	for (const character of s)
+		result.push(character.charCodeAt(0));
+	return new Uint8Array(result);
 }
 
-function getHash(array: Uint8Array) {
-	let multiplier = 1;
-	let sum = 0;
-	for (const item of array) {
-		multiplier *= array.length;
-		sum += item * multiplier;
-	}
-	return sum;
+function compareOrdered(source: Uint8Array, order: Uint8Array, target: Uint8Array): boolean {
+	for (let i = 0; i < order.length; ++i)
+		if (source[order[i]] !== target[i])
+			return false;
+	return true;
 }
 
 class Scrambler {
 	private bufferSequence: Uint8Array;
 	public readonly visitedSequences: Set<number> = new Set();
 
-	constructor(public sequence: Uint8Array, public readonly desiredSequence: Uint8Array) {
+	constructor(
+		public sequence: Uint8Array,
+		public readonly sourceText: Uint8Array,
+		public readonly desiredText: Uint8Array,
+	) {
 		this.bufferSequence = new Uint8Array(sequence.length).fill(0);
 	}
 
@@ -61,43 +44,24 @@ class Scrambler {
 		if (this.check())
 			return true;
 		for (let i = start + 1; i < end; ++i) {
-			const hashInitial = getHash(this.sequence);
-
 			let middle = i;
 			if (this.next(start, middle, depth + 1))
 				return true;
 			if (this.next(middle, end, depth + 1))
 				return true;
 
-			const hashBefore = getHash(this.sequence);
-			if (hashInitial !== hashBefore)
-				throw new Error('initial hash error');
 			middle = this.swap(start, middle, end);
-			if (depth === 0)
-				console.log(this.sequence.join());
 			if (this.next(start, middle, depth + 1))
 				return true;
 			if (this.next(middle, end, depth + 1))
 				return true;
 			this.swap(start, middle, end);
-			const hashAfter = getHash(this.sequence);
-
-			if (hashBefore !== hashAfter)
-				throw new Error('hash error');
 		}
 		return false;
 	}
 
-	check() {
-		let found = true;
-		for (let i = 0; i < this.desiredSequence.length; ++i)
-			if (this.desiredSequence[i] !== this.sequence[i]) {
-				found = false;
-				break;
-			}
-		if (found)
-			console.log('found', this.sequence);
-		return found;
+	private check() {
+		return compareOrdered(this.sourceText, this.sequence, this.desiredText);
 	}
 
 	private swap(start: number, middle: number, end: number): number {
@@ -115,6 +79,16 @@ class Scrambler {
 		this.sequence = buffer;
 		return start + segmentLength - offset;
 	}
+}
+
+function getHash(array: Uint8Array) {
+	let multiplier = 1;
+	let sum = 0;
+	for (const item of array) {
+		multiplier *= array.length;
+		sum += item * multiplier;
+	}
+	return sum;
 }
 
 export const isScrambleEx = isScramble;
