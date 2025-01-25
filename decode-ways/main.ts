c72e17a94450cc52ccdccc0eb53b89a72bcff64c
hinst <hinstance@yandex.ru>
2025-01-25 21:21:19 +0100

    Added cache. Passed all test cases online for task 91. Decode Ways

--- decode-ways/main.ts
+++ decode-ways/main.ts
@@ -1,24 +1,33 @@
-const LETTER_MAP = new Map<string, string>();
+const KEYS: string[] = [];
 {
+	const LETTER_MAP = new Map<string, string>();
 	const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	let letterIndex = 0;
 	for (const letter of LETTERS) {
 		++letterIndex;
 		LETTER_MAP.set('' + letterIndex, letter);
+		KEYS.push('' + letterIndex);
 	}
 }
 
 function numDecodings(s: string): number {
-	return decode(s, 0);
+	const cache = new Map<number, number>();
+	return decode(cache, s, 0);
 }
 
-function decode(text: string, index: number): number {
+function decode(cache: Map<number, number>, text: string, index: number): number {
 	if (index >= text.length)
 		return 1;
 	let counter = 0;
-	for (const key of LETTER_MAP.keys()) {
+	for (const key of KEYS) {
 		if (text.startsWith(key, index)) {
-			counter += decode(text, index + key.length);
+			const newIndex = index + key.length;
+			let count = cache.get(newIndex);
+			if (count === undefined) {
+				count = decode(cache, text, newIndex);
+				cache.set(newIndex, count);
+			}
+			counter += count;
 		}
 	}
 	return counter;
@@ -27,5 +36,5 @@ function decode(text: string, index: number): number {
 export const numDecodingsEx = numDecodings;
 
 if (import.meta.main) {
-	console.log(numDecodings('12'));
+	console.log(numDecodings('111111111111111111111111111111111111111111111'));
 }
