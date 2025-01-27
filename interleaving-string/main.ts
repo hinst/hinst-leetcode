8b7b7ba17f001c77acd6fe6589fc342fb1b1bd52
hinst <hinstance@yandex.ru>
2025-01-27 22:39:49 +0100

    Removing depth

--- interleaving-string/main.ts
+++ interleaving-string/main.ts
@@ -9,15 +9,14 @@ function isInterleave(s1: string, s2: string, combinedText: string): boolean {
 function advance(
 	s1: number[], index1: number,
 	s2: number[], index2: number,
-	combinedText: number[], combinedIndex: number,
-	depth: number = 0,
+	combinedText: number[], combinedIndex: number
 ): boolean {
 	if (combinedIndex >= combinedText.length)
 		return index1 === s1.length && index2 === s2.length;
 	const canAdvance1 = combinedText[combinedIndex] === s1[index1] &&
-		advance(s1, index1 + 1, s2, index2, combinedText, combinedIndex + 1, depth + 1);
+		advance(s1, index1 + 1, s2, index2, combinedText, combinedIndex + 1);
 	const canAdvance2 = combinedText[combinedIndex] === s2[index2] &&
-		advance(s1, index1, s2, index2 + 1, combinedText, combinedIndex + 1, depth + 1);
+		advance(s1, index1, s2, index2 + 1, combinedText, combinedIndex + 1);
 	return canAdvance1 || canAdvance2;
 }
 
