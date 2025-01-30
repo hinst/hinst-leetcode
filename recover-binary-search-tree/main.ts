ecf23cc3207aa939e420cb9eec3956995e250c99
hinst <hinstance@yandex.ru>
2025-01-30 01:34:20 +0100

    Advanded up to test case 1296 of 1919

--- recover-binary-search-tree/main.ts
+++ recover-binary-search-tree/main.ts
@@ -2,16 +2,20 @@
  * Definition for a binary tree node.
  */
 class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
     }
 }
 
+
+class TreeNodeEx extends TreeNode {
+	parent?: TreeNode;
+}
 
 class TreeCover {
 	private allNodes: TreeNode[] = [];
@@ -22,6 +26,18 @@ class TreeCover {
 
 	recover() {
 		this.build([this.root]);
+		if (!this.wrongNode)
+			throw new Error('Wrong node is missing');
+		for (const node of this.allNodes) {
+			if (node === this.wrongNode)
+				continue;
+			this.swap(node, this.wrongNode);
+			const isValid = this.checkValidNode(node) && this.checkValidNode(this.wrongNode);
+			if (isValid) {
+				break;
+			}
+			this.swap(node, this.wrongNode);
+		}
 	}
 
 	private build(path: TreeNode[], depth: number = 0): boolean {
@@ -46,9 +62,51 @@ class TreeCover {
 				isValid = false;
 			path.pop();
 		}
-		if (!isValid)
+		if (!isValid && !this.wrongNode)
 			this.wrongNode = lastNode;
+		if (path.length > 1)
+			(lastNode as TreeNodeEx).parent = path[path.length - 2];
 		return isValid;
+	}
+
+	private checkValidNode(node: TreeNode): boolean {
+		const path: TreeNode[] = [node];
+		let parentNode = (node as TreeNodeEx).parent;
+		while (parentNode) {
+			path.unshift(parentNode);
+			parentNode = (parentNode as TreeNodeEx).parent;
+		}
+		return this.checkValidPath(path);
+	}
+
+	private checkValidPath(path: TreeNode[], depth: number = 0): boolean {
+		const lastNode = path[path.length - 1];
+		let isValid = true;
+		for (let i = 0; i < path.length - 1 && isValid; ++i) {
+			const isLeft = path[i].left === path[i + 1];
+			isValid = isLeft
+				? lastNode.val < path[i].val
+				: path[i].val < lastNode.val;
+		}
+		if (isValid) {
+			if (lastNode.left) {
+				path.push(lastNode.left);
+				isValid = this.checkValidPath(path, depth + 1);
+				path.pop();
+			}
+			if (lastNode.right && isValid) {
+				path.push(lastNode.right);
+				isValid = this.checkValidPath(path, depth + 1);
+				path.pop();
+			}
+		}
+		return isValid;
+	}
+
+	private swap(a: TreeNode, b: TreeNode) {
+		const buffer = b.val;
+		b.val = a.val;
+		a.val = buffer;
 	}
 }
 
