83df54aff721d96cfd79f765ab6f2d81f3cb12bc
hinst <hinstance@yandex.ru>
2025-06-15 20:48:32 +0200

    Try all possible preorder trees, work in progress

--- construct-binary-tree-from-preorder-and-inorder-traversal/main.ts
+++ construct-binary-tree-from-preorder-and-inorder-traversal/main.ts
@@ -2,7 +2,44 @@ import { TreeNode } from '../treeNode.ts';
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
 	const node = new TreeNode(preorder[0]);
-	return node;
+	return buildNext(node, node, preorder, inorder, 1);
+}
+
+function buildNext(root: TreeNode, node: TreeNode, preorder: number[], inorder: number[], index: number): TreeNode | null {
+	if (index >= preorder.length) {
+		console.log(root.toString());
+		return checkArraysEqual(inorderTraversal(root), inorder) ? root : null;
+	}
+	const value = preorder[index];
+	const newNode = new TreeNode(value);
+	node.left = newNode;
+	{
+		const answer = buildNext(root, newNode, preorder, inorder, index + 1);
+		if (answer)
+			return answer;
+	}
+	node.left = null;
+	node.right = newNode;
+	return buildNext(root, newNode, preorder, inorder, index + 1);
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+	const values: number[] = [];
+	if (root) {
+		values.push(...inorderTraversal(root.left));
+		values.push(root.val);
+		values.push(...inorderTraversal(root.right));
+	}
+	return values;
+}
+
+function checkArraysEqual(a: number[], b: number[]): boolean {
+	if (a.length !== b.length)
+		return false;
+	for (let i = 0; i < a.length; ++i)
+		if (a[i] !== b[i])
+			return false;
+	return true;
 }
 
 export const buildTreeEx = buildTree;
