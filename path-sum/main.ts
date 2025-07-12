125481fdb194016728258361ef3683f26012d947
hinst <hinstance@yandex.ru>
2025-07-12 14:19:11 +0200

    Adding task 112. Path Sum

---
+++ path-sum/main.ts
@@ -0,0 +1,13 @@
+import { TreeNode } from '../treeNode.ts';
+
+function hasPathSum(root: TreeNode | null, targetSum: number, currentSum = 0): boolean {
+	if (!root)
+		return false;
+	currentSum += root.val;
+	if (currentSum === targetSum)
+		return true;
+	return hasPathSum(root.left, targetSum, currentSum) || hasPathSum(root.right, targetSum, currentSum);
+}
+
+if (import.meta.main) {
+}
