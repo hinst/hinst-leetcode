35fa2e80ade5d95b9f4a9110475d76ef6f47991b
hinst <hinstance@yandex.ru>
2025-07-13 15:53:06 +0200

    Advanced up to test case 42 of 45 and got Time Limit Exceeded

--- triangle/main.ts
+++ triangle/main.ts
@@ -5,9 +5,15 @@ More formally, if you are on index i on the current row, you may move
 to either index i or index i + 1 on the next row.
 */
 
-function minimumTotal(triangle: number[][]): number {
-
+function minimumTotal(triangle: number[][], sum = 0, x = 0, y = 0): number {
+	if (y >= triangle.length)
+		return sum;
+	sum += triangle[y][x];
+	y += 1;
+	return Math.min(minimumTotal(triangle, sum, x, y), minimumTotal(triangle, sum, x + 1, y));
 }
 
 if (import.meta.main) {
+	const triangle = [[2],[3,4],[6,5,7],[4,1,8,3]];
+	console.log(minimumTotal(triangle));
 }
