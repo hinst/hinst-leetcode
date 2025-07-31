8a621f805cae3b197901c0b6ff6dd13a44cbd9c1
hinst <hinstance@yandex.ru>
2025-07-31 23:20:13 +0200

    Using new approach: recursion with cache. Passed unit tests online up to 201 of 214

--- best-time-to-buy-and-sell-stock-iii/main.ts
+++ best-time-to-buy-and-sell-stock-iii/main.ts
@@ -10,23 +10,25 @@ enum Status {
 	SOLD_2
 }
 
-function max(items: (number | undefined)[]) {
-	return items.filter(item => item !== undefined)[0];
+function max(level: number, items: (number | undefined)[]) {
+	const filteredItems = items.filter(item => item !== undefined);
+	return filteredItems.length ? Math.max(...filteredItems) : undefined;
 }
 
 class Finder {
-	cache = new Map<number, Map<Status, number>>();
+	cache = new Map<number, Map<Status, number | null>>();
 
 	constructor(readonly prices: number[]) {
 	}
 
 	search(index: number, status: Status): number | undefined {
-		console.log(' '.repeat(index) + ' ' + status);
+		const profit = this.searchInternal(index, status);
+		return profit;
+	}
+
+	private searchInternal(index: number, status: Status): number | undefined {
 		if (this.prices.length <= index)
-			return undefined;
-		let storedProfit = this.cache.get(index)?.get(status);
-		if (storedProfit !== undefined)
-			return storedProfit;
+			return [Status.BEGINNING, Status.BEGINNING_2].includes(status) ? 0 : undefined;
 		const nextIndex = index + 1;
 		switch (status) {
 			case Status.BEGINNING: {
@@ -37,7 +39,7 @@ class Finder {
 					: undefined;
 				// skip
 				const profitSkip = this.search(nextIndex, status);
-				return max([profitBuy, profitSkip]);
+				return max(index, [profitBuy, profitSkip]);
 			}
 			case Status.BOUGHT: {
 				// sell
@@ -47,24 +49,24 @@ class Finder {
 					: undefined;
 				// skip
 				const profitSkip = this.search(nextIndex, status);
-				return max([profitSell, profitSkip]);
+				return max(index, [profitSell, profitSkip]);
 			}
 			case Status.BEGINNING_2: {
 				// buy
 				const futureProfit = this.search(nextIndex, Status.BOUGHT_2);
 				const profitBuy = futureProfit !== undefined
 					? - this.prices[index] + futureProfit
-					: undefined;
+					: 0;
 				// skip
 				const profitSkip = this.search(nextIndex, status);
-				return max([profitBuy, profitSkip]);
+				return max(index, [profitBuy, profitSkip]);
 			}
 			case Status.BOUGHT_2: {
 				// sell
 				const profitSell = this.prices[index];
 				// skip
 				const futureProfit = this.search(nextIndex, status);
-				return max([profitSell, futureProfit]);
+				return max(index, [profitSell, futureProfit]);
 			}
 			case Status.SOLD_2:
 				return 0;
@@ -75,7 +77,7 @@ class Finder {
 export const maxProfitEx = maxProfit;
 
 if (import.meta.main) {
-	const prices = [1, 2, 3];
+	const prices = [1,2,3,4];
 	const message = 'prices[' + prices.length + ']';
 	console.time(message);
 	console.log(maxProfit(prices));
