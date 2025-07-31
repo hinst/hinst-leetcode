5e4058b4c387fd248c0d9736b7a813043d4f35e2
hinst <hinstance@yandex.ru>
2025-07-31 23:02:21 +0200

    Using new approach: recursion with cache, work in progress.

--- best-time-to-buy-and-sell-stock-iii/main.ts
+++ best-time-to-buy-and-sell-stock-iii/main.ts
@@ -1,94 +1,83 @@
 function maxProfit(prices: number[]): number {
-	let maxProfit = 0;
-	for (let splitIndex = 0; splitIndex < prices.length; ++splitIndex) {
-		const leftProfit = findProfit(prices, 0, splitIndex);
-		const rightProfit = findProfit(prices, splitIndex, prices.length);
-		const totalProfit = leftProfit + rightProfit;
-		if (maxProfit < totalProfit)
-			maxProfit = totalProfit;
-	}
-	return maxProfit;
+	return new Finder(prices).search(0, Status.BEGINNING) || 0;
 }
 
-class SortedItem {
-	constructor(
-		public readonly value: number,
-		public readonly index: number,
-		public previous?: SortedItem,
-		public next?: SortedItem) {
-	}
-
-	toString() {
-		let text = '' + this.value + '[' + this.index + ']';
-		if (this.next)
-			text += ', ' + this.next.toString();
-		return text;
-	}
+enum Status {
+	BEGINNING,
+	BOUGHT,
+	BEGINNING_2,
+	BOUGHT_2,
+	SOLD_2
 }
 
-class SortedIndex {
-	head?: SortedItem;
-	/** Map<sourceIndex, SortedItem> */
-	indexMap = new Map<number, SortedItem>();
+function max(items: (number | undefined)[]) {
+	return items.filter(item => item !== undefined)[0];
+}
 
-	constructor(sources: number[], from: number, to: number) {
-		const items: SortedItem[] = [];
-		for (let index = from; index < to; ++index) {
-			const item = new SortedItem(sources[index], index)
-			this.indexMap.set(index, item);
-			items.push(item);
-		}
-		items.sort((a, b) => b.value - a.value);
-		let previousItem: SortedItem | undefined;
-		for (const item of items) {
-			if (previousItem) {
-				previousItem.next = item;
-				item.previous = previousItem;
-			} else {
-				this.head = item;
+class Finder {
+	cache = new Map<number, Map<Status, number>>();
+
+	constructor(readonly prices: number[]) {
+	}
+
+	search(index: number, status: Status): number | undefined {
+		console.log(' '.repeat(index) + ' ' + status);
+		if (this.prices.length <= index)
+			return undefined;
+		let storedProfit = this.cache.get(index)?.get(status);
+		if (storedProfit !== undefined)
+			return storedProfit;
+		const nextIndex = index + 1;
+		switch (status) {
+			case Status.BEGINNING: {
+				// buy
+				const futureProfit = this.search(nextIndex, Status.BOUGHT);
+				const profitBuy = futureProfit !== undefined
+					? - this.prices[index] + futureProfit
+					: undefined;
+				// skip
+				const profitSkip = this.search(nextIndex, status);
+				return max([profitBuy, profitSkip]);
 			}
-			previousItem = item;
+			case Status.BOUGHT: {
+				// sell
+				const futureProfit = this.search(nextIndex, Status.BEGINNING_2);
+				const profitSell = futureProfit !== undefined
+					? this.prices[index] + futureProfit
+					: undefined;
+				// skip
+				const profitSkip = this.search(nextIndex, status);
+				return max([profitSell, profitSkip]);
+			}
+			case Status.BEGINNING_2: {
+				// buy
+				const futureProfit = this.search(nextIndex, Status.BOUGHT_2);
+				const profitBuy = futureProfit !== undefined
+					? - this.prices[index] + futureProfit
+					: undefined;
+				// skip
+				const profitSkip = this.search(nextIndex, status);
+				return max([profitBuy, profitSkip]);
+			}
+			case Status.BOUGHT_2: {
+				// sell
+				const profitSell = this.prices[index];
+				// skip
+				const futureProfit = this.search(nextIndex, status);
+				return max([profitSell, futureProfit]);
+			}
+			case Status.SOLD_2:
+				return 0;
 		}
 	}
-
-	deleteByIndex(index: number) {
-		const item = this.indexMap.get(index);
-		if (!item)
-			return;
-		if (item.previous)
-			item.previous.next = item.next;
-		if (item === this.head)
-			this.head = item.next;
-		if (item.next)
-			item.next.previous = item.previous;
-		this.indexMap.delete(index);
-	}
-}
-
-function findProfit(prices: number[], from: number, to: number): number {
-	let maxProfit = 0;
-	if (!prices.length)
-		return 0;
-	const sortedIndex = new SortedIndex(prices, from, to);
-	for (let i = from; i < to; ++i) {
-		const currentPrice = prices[i];
-		sortedIndex.deleteByIndex(i);
-		const nextPrice = sortedIndex.head?.value;
-		if (nextPrice === undefined)
-			break;
-		const profit = nextPrice - currentPrice;
-		if (maxProfit < profit)
-			maxProfit = profit;
-	}
-	return maxProfit;
 }
 
 export const maxProfitEx = maxProfit;
 
 if (import.meta.main) {
-	const prices = [3,3,5,0,0,3,1,4];
-	const sortedIndex = new SortedIndex(prices, 0, prices.length);
-	console.log(sortedIndex.head?.toString());
-	sortedIndex.deleteByIndex(0);
-	console.log(sortedIndex.head?.toString());
+	const prices = [1, 2, 3];
+	const message = 'prices[' + prices.length + ']';
+	console.time(message);
+	console.log(maxProfit(prices));
+	console.timeEnd(message);
 }
