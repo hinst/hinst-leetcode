d52eebb014cda91c770f7d0294330a69eeb02def
hinst <hinstance@yandex.ru>
2025-08-01 19:39:30 +0200

    Adding task 124. Binary Tree Maximum Path Sum

---
+++ binary-tree-maximum-path-sum/main.ts
@@ -0,0 +1,11 @@
+import { TreeNode } from '../treeNode.ts';
+
+function maxPathSum(root: TreeNode | null): number {
+
+}
+
+
+export const maxPathSumEx = maxPathSum;
+
+if (import.meta.main) {
+}
---
+++ binary-tree-maximum-path-sum/main_test.ts
@@ -0,0 +1,5 @@
+import { assertEquals } from '@std/assert';
+import { maxPathSumEx } from './main.ts';
+
+Deno.test(function maxPathSum() {
+});
