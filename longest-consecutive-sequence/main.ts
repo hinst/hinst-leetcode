1ad513f355bd02c8d99574271d504d7c6cafa2a5
hinst <hinstance@yandex.ru>
2025-08-03 16:33:51 +0200

    Completed task 128. Longest Consecutive Sequence

--- longest-consecutive-sequence/main.ts
+++ longest-consecutive-sequence/main.ts
@@ -1,35 +1,63 @@
-const map: Map<number, {count: number}> = new Map();
+class Sequence {
+	beginning: number = 0;
+	ending: number = 0;
+	duplicateCount: number = 0;
+	get length() {
+		return this.ending - this.beginning + 1;
+	}
+}
+
+const map: Map<number, Sequence> = new Map();
 
 function longestConsecutive(numbers: number[]): number {
 	map.clear();
 	let max = 0;
+	function updateMax(sequence: Sequence) {
+		if (max < sequence.length)
+			max = sequence.length;
+	}
 	for (const value of numbers) {
-		let item = map.get(value);
-		if (!item) {
-			item = {count: 1};
-			map.set(value, item);
-		} else {
-			++item.count;
+		const centralSequence = map.get(value);
+		if (centralSequence) {
+			++centralSequence.duplicateCount;
+			updateMax(centralSequence);
+			continue;
 		}
-
-		const left = map.get(value - 1);
-		if (left) {
-			item.count += left.count;
-			map.set(value - 1, item);
+		const leftSequence = map.get(value - 1);
+		const rightSequence = map.get(value + 1);
+		if (leftSequence && !rightSequence) {
+			leftSequence.ending = value;
+			map.set(value, leftSequence);
+			updateMax(leftSequence);
+			continue;
 		}
-		const right = map.get(value + 1);
-		if (right) {
-			item.count += right.count;
-			map.set(value + 1, item);
+		if (!leftSequence && rightSequence) {
+			rightSequence.beginning = value;
+			map.set(value, rightSequence);
+			updateMax(rightSequence);
+			continue;
 		}
-
-		if (max < item.count)
-			max = item.count;
+		if (leftSequence && rightSequence) {
+			map.set(value, leftSequence);
+			leftSequence.ending = rightSequence.ending;
+			for (let i = rightSequence.beginning; i <= rightSequence.ending; ++i)
+				map.set(i, leftSequence);
+			updateMax(leftSequence);
+			continue;
+		}
+		if (!leftSequence && !rightSequence) {
+			const sequence = new Sequence();
+			sequence.beginning = sequence.ending = value;
+			map.set(value, sequence);
+			updateMax(sequence);
+			continue;
+		}
 	}
 	return max;
 }
 
 if (import.meta.main) {
-	const nums = [100,4,200,1,3,2];
+	const nums = [1,0,1,2];
+	console.log(nums.sort());
 	console.log(longestConsecutive(nums));
 }
