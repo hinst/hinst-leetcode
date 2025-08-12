a8c0dfc718e9f8708f8f2c5049bc8bc67f8b4a54
hinst <hinstance@yandex.ru>
2025-08-12 15:36:39 +0200

    Completing task 131. Palindrome Partitioning

--- palindrome-partitioning/main.ts
+++ palindrome-partitioning/main.ts
@@ -1,15 +1,42 @@
 function partition(s: string): string[][] {
     const p = new Partition(s);
+	return p.find();
 }
 
+type Pair = [number, number];
+
 class Partition {
-	readonly chains: string[][] = [];
+	private readonly chains: string[][] = [];
 
 	constructor(private readonly text: string) {
 	}
 
-	next(index: number = 0) {
+	public find() {
+		this.next(0, []);
+		return this.chains;
+	}
 
+	private next(index: number, chain: Pair[]) {
+		if (index >= this.text.length) {
+			this.chains.push(chain.map(pair => this.text.substring(pair[0], pair[1])));
+		}
+		for (let i = index; i < this.text.length; ++i) {
+			if (this.check(index, i)) {
+				chain.push([index, i + 1]);
+				this.next(i + 1, chain);
+				chain.pop();
+			}
+		}
+	}
+
+	check(a: number, b: number) {
+		while (a < b) {
+			if (this.text[a] !== this.text[b])
+				return false;
+			++a;
+			--b;
+		}
+		return true;
 	}
 }
 
@@ -17,4 +44,6 @@ class Partition {
 export const partitionEx = partition;
 
 if (import.meta.main) {
+	const s = 'aab';
+	console.log(partition(s));
 }
