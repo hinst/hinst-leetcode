038d72e24438da3ad8d44341951e29735f7435d7
hinst <hinstance@yandex.ru>
2025-08-14 14:01:48 +0200

    Advanced to test case 24 of 37 and got Time Limit Exceeded

--- palindrome-partitioning-ii/main.ts
+++ palindrome-partitioning-ii/main.ts
@@ -1,7 +1,50 @@
 function minCut(s: string): number {
-	
+	const p = new Partition(s);
+	return p.find();	
 }
 
+type Pair = [number, number];
+
+class Partition {
+	private readonly chains: string[][] = [];
+
+	constructor(private readonly text: string) {
+	}
+
+	public find(): number {
+		return this.next(0, []);
+	}
+
+	private next(index: number, chain: Pair[]): number {
+		if (index >= this.text.length) {
+			return chain.length - 1;
+		}
+		let min = -1;
+		for (let i = index; i < this.text.length; ++i) {
+			if (this.check(index, i)) {
+				chain.push([index, i + 1]);
+				const length =  this.next(i + 1, chain);
+				if (-1 === min || length < min)
+					min = length;
+				chain.pop();
+			}
+		}
+		return min;
+	}
+
+	check(a: number, b: number) {
+		while (a < b) {
+			if (this.text[a] !== this.text[b])
+				return false;
+			++a;
+			--b;
+		}
+		return true;
+	}
+}
+
+
 if (import.meta.main) {
-	console.log("Add 2 + 3 =", add(2, 3));
+	const s = 'aab';
+	console.log(minCut(s));
 }
