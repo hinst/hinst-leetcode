c7624ce370d829909911a4fdf1dea8d59ddbd726
hinst <hinstance@yandex.ru>
2025-08-18 18:07:09 +0200

    Adding task Gas Station

---
+++ gas-station/main.ts
@@ -0,0 +1,10 @@
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+	for (let i = 0; i < gas.length; ++i) {
+		
+	}
+}
+
+if (import.meta.main) {
+	const gas = [1,2,3,4,5], cost = [3,4,5,1,2];
+	console.log(canCompleteCircuit(gas, cost));
+}
