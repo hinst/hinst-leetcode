913e2d9a61685f47a0c142266f0230b74f24fbba
hinst <hinstance@yandex.ru>
2025-08-18 11:05:07 +0200

    Completed task 133. Clone Graph

--- clone-graph/main.ts
+++ clone-graph/main.ts
@@ -9,9 +9,21 @@ class _Node {
 }
 
 
-function cloneGraph(node: _Node | null): _Node | null {
-	
-};
+function cloneGraph(node: _Node | null, nodeMap = new Map<number, _Node>()): _Node | null {
+	if (!node)
+		return node;
+	let newNode = nodeMap.get(node.val);
+	if (newNode)
+		return newNode;
+	newNode = new _Node(node.val);
+	nodeMap.set(newNode.val, newNode);
+	for (const neighbor of node.neighbors) {
+		const newNeighbor = cloneGraph(neighbor, nodeMap);
+		if (newNeighbor)
+			newNode.neighbors.push(newNeighbor);
+	}
+	return newNode;
+}
 
 if (import.meta.main) {
 }
