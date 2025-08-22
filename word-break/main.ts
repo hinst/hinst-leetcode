a46477cb47dbdb97c5c3e639f1801db2194020e4
hinst <hinstance@yandex.ru>
2025-08-22 19:11:15 +0200

    Passed test cases online up to 35 / 47

--- word-break/main.ts
+++ word-break/main.ts
@@ -1,6 +1,19 @@
-function wordBreak(s: string, wordDict: string[]): boolean {
-
+function wordBreak(s: string, wordDict: string[], index = 0): boolean {
+	if (index === s.length)
+		return true;
+	for (const word of wordDict) {
+		if (s.indexOf(word, index) === index) {
+			if (wordBreak(s, wordDict, index + word.length))
+				return true;
+		}
+	}
+	return false;
 }
 
+
+export const wordBreakEx = wordBreak;
+
 if (import.meta.main) {
+	const s = "leetcode", wordDict = ["leet","code"];
+	console.log(wordBreak(s, wordDict));
 }
--- word-break/main_test.ts
+++ word-break/main_test.ts
@@ -1,6 +1,15 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { wordBreakEx } from './main.ts';
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function wordBreak() {
+	const s = "leetcode", wordDict = ["leet","code"];
+	assertEquals(wordBreakEx(s, wordDict), true);
+});
+Deno.test(function wordBreak() {
+	const s = "applepenapple", wordDict = ["apple","pen"]
+	assertEquals(wordBreakEx(s, wordDict), true);
+});
+Deno.test(function wordBreak() {
+	const s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+	assertEquals(wordBreakEx(s, wordDict), false);
 });
