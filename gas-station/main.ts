1738e383b3caa2305a58fa2da675def52702ddde
hinst <hinstance@yandex.ru>
2025-08-22 16:13:13 +0200

    Completed task: Gas Station

--- gas-station/main.ts
+++ gas-station/main.ts
@@ -1,7 +1,21 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
+	const limit = gas.length * 2;
 	for (let i = 0; i < gas.length; ++i) {
-		
+		let sum = 0;
+		let step: number;
+		for (step = i; step < limit; ++step) {
+			const limitedStep = step % gas.length;
+			sum += gas[limitedStep];
+			sum -= cost[limitedStep];
+			if (sum < 0) {
+				break;
+			}
+			if (limitedStep !== step && limitedStep == i)
+				return i;
+		}
+		i = step;
 	}
+	return -1;
 }
 
 if (import.meta.main) {
