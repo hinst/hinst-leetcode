dfc0461707ede3fd513a07a755c2b22353e149ba
hinst <hinstance@yandex.ru>
2025-08-22 19:26:37 +0200

    Completed task 139. Word Break

--- word-break/main.ts
+++ word-break/main.ts
@@ -1,13 +1,37 @@
 function wordBreak(s: string, wordDict: string[], index = 0): boolean {
-	if (index === s.length)
-		return true;
-	for (const word of wordDict) {
-		if (s.indexOf(word, index) === index) {
-			if (wordBreak(s, wordDict, index + word.length))
-				return true;
-		}
+	const instance = new WordBreak(s, wordDict);
+	return instance.next(0);
+}
+
+class WordBreak {
+	private readonly cache = new Map<number, boolean>();
+
+	constructor(
+		private readonly s: string,
+		private readonly wordDict: string[]
+	) {
 	}
-	return false;
+
+	nextCached(index: number) {
+		let cached = this.cache.get(index);
+		if (cached !== undefined)
+			return cached;
+		cached = this.next(index);
+		this.cache.set(index, cached);
+		return cached;
+	}
+
+	next(index: number): boolean {
+		if (index === this.s.length)
+			return true;
+		for (const word of this.wordDict) {
+			if (this.s.indexOf(word, index) === index) {
+				if (this.nextCached(index + word.length))
+					return true;
+			}
+		}
+		return false;
+	}
 }
 
 
