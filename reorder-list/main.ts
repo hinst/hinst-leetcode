3f95d8e8b2c6831415868851328a2bec5758e452
hinst <hinstance@yandex.ru>
2025-09-01 18:42:38 +0200

    Optimized task 142. Linked List Cycle II
    Avoid modifying nodes array

--- reorder-list/main.ts
+++ reorder-list/main.ts
@@ -5,14 +5,16 @@ function reorderList(head: ListNode | null): void {
 	const nodes: ListNode[] = [];
 	for (let node = head; node; node = node.next)
 		nodes.push(node);
-	const newNodes: ListNode[] = [];
 	let previousNode: ListNode | null = null;
 	let toggle = true;
-	while (nodes.length) {
-		const nextNode = (toggle ? nodes.shift() : nodes.pop()) || null;
+	let beginning = 0;
+	let ending = nodes.length - 1;
+	while (beginning <= ending) {
+		const nextNode = (toggle ? nodes[beginning++] : nodes[ending--]) || null;
 		if (previousNode)
 			previousNode.next = nextNode;
 		previousNode = nextNode;
+		toggle = !toggle;
 	}
 	if (previousNode)
 		previousNode.next = null;
