881f9362727a8266e4eeddefd753660413cbd24e
hinst <hinstance@yandex.ru>
2025-09-15 15:34:51 +0200

    Completed task 160

--- intersection-of-two-linked-lists/main.ts
+++ intersection-of-two-linked-lists/main.ts
@@ -1,7 +1,17 @@
 import { ListNode } from '../listNode.ts';
 
 function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-
+	const nodesA = new Set<ListNode>();
+	while (headA) {
+		nodesA.add(headA);
+		headA = headA.next;
+	}
+	while (headB) {
+		if (nodesA.has(headB))
+			return headB;
+		headB = headB.next;
+	}
+	return null;
 }
 
 if (import.meta.main) {
