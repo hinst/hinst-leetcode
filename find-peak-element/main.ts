858dd0a6ddab58e449a51b943ee10d2b5787d6c1
hinst <hinstance@yandex.ru>
2025-09-16 11:08:08 +0200

    Completed task 162 with linear time. However, the description text asks for logarithmic time

--- find-peak-element/main.ts
+++ find-peak-element/main.ts
@@ -1,7 +1,10 @@
 function findPeakElement(nums: number[]): number {
-    
+    for (let i = 0; i < nums.length; ++i) {
+		if ((nums[i - 1] < nums[i] || nums[i - 1] === undefined) && (nums[i] > nums[i + 1]) || nums[i + 1] === undefined)
+			return i;
+	}
+	return -1;
 }
 
 if (import.meta.main) {
-	console.log("Add 2 + 3 =", add(2, 3));
 }
