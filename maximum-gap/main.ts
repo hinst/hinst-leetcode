ef6bd4e771e445bcb03a7fcaa291c4d9a65e978c
hinst <hinstance@yandex.ru>
2025-10-03 13:14:39 +0200

    Passed online test cases for task 164. Maximum Gap, ignoring "linear time" constraint from the description

--- maximum-gap/main.ts
+++ maximum-gap/main.ts
@@ -1,6 +1,15 @@
-function maximumGap(nums: number[]): number {
-
-};
+function maximumGap(items: number[]): number {
+	let maxDiff = 0;
+	if (items.length < 2)
+		return maxDiff;
+	items.sort((a, b) => a - b);
+	for (let i = 1; i < items.length; ++i) {
+		const diff = items[i] - items[i - 1];
+		if (maxDiff < diff)
+			maxDiff = diff;
+	}
+	return maxDiff;
+}
 
 if (import.meta.main) {
 }
