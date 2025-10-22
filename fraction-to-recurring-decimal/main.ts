aae9a518e3bd2be21313df15a4c55b81d7e2fa9a
hinst <hinstance@yandex.ru>
2025-10-22 11:58:33 +0200

    Division is now ready, the next step is to find the repeated part in the array

--- fraction-to-recurring-decimal/main.ts
+++ fraction-to-recurring-decimal/main.ts
@@ -1,7 +1,28 @@
 function fractionToDecimal(numerator: number, denominator: number): string {
-	return 'TODO';
+	let result = divide(numerator, denominator).join('');
+	if (result.length > 1)
+		result = result[0] + '.' + result.substring(1);
+	return result;
+}
+
+const LENGTH_LIMIT = Math.pow(10, 4);
+
+function divide(a: number, b: number): number[] {
+	const result: number[] = [];
+	while (a > 0 && result.length < LENGTH_LIMIT) {
+		if (a < b) {
+			result.push(0);
+			a *= 10;
+			continue;
+		}
+		const whole = Math.trunc(a / b);
+		const remainder = a - whole * b;
+		result.push(whole);
+		a = remainder * 10;
+	}
+	return result;
 }
 
 if (import.meta.main) {
-	console.log(fractionToDecimal(1, 2));
+	console.log(fractionToDecimal(4, 333));
 }
