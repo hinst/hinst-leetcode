6a7d4e9e85176a27879aad7ba185522906caaa1d
hinst <hinstance@yandex.ru>
2025-10-31 15:16:45 +0100

    Adding pattern detection and unit tests for task 166. Fraction to Recurring Decimal

--- fraction-to-recurring-decimal/main.ts
+++ fraction-to-recurring-decimal/main.ts
@@ -1,15 +1,28 @@
 const LENGTH_LIMIT = Math.pow(10, 4);
-const FRACTIONAL_DELIMITER = -2;
+const FRACTIONAL_DELIMITER = -2,
+	PATTERN_BEGINNING = -3,
+	PATTERN_ENDING = -4;
 
 function fractionToDecimal(numerator: number, denominator: number): string {
 	let result = divide(numerator, denominator);
 	result = collapse(result);
-	let text = result
-		.map(item => item === FRACTIONAL_DELIMITER ? '.' : item.toString())
-		.join('');
+	let text = result.map(itemToCharacter).join('');
 	if (text.startsWith('.'))
 		text = '0' + text;
 	return text;
+}
+
+function itemToCharacter(item: number): string {
+	switch (item) {
+		case FRACTIONAL_DELIMITER:
+			return '.';
+		case PATTERN_BEGINNING:
+			return '(';
+		case PATTERN_ENDING:
+			return ')';
+		default:
+			return item.toString();
+	}
 }
 
 function divide(a: number, b: number): number[] {
@@ -38,11 +51,51 @@ function collapse(items: number[]): number[] {
 		if (item === FRACTIONAL_DELIMITER || fractionalLength >= 0)
 			fractionalLength++;
 	}
-	fractionalLength = Math.min(0, fractionalLength);
-	for (let repeatingLength = fractionalLength / 2; repeatingLength > 0 ; --repeatingLength) {
+	++fractionalLength;
+	let bestPatternCount = 0;
+	let bestPatternLength = 0;
+	for (let patternLength = Math.trunc(fractionalLength / 2); patternLength > 0 ; --patternLength) {
+		const patternCount = checkPattern(items, patternLength);
+		if (bestPatternCount < patternCount) {
+			bestPatternCount = patternCount;
+			bestPatternLength = patternLength;
+		}
+	}
+	if (bestPatternCount > 1) {
+		const patternStartIndex = items.length - bestPatternLength * bestPatternCount;
+		const pattern = items.slice(patternStartIndex, patternStartIndex + bestPatternLength);
+		items = items.slice(0, patternStartIndex);
+		items.push(PATTERN_BEGINNING);
+		items.push(...pattern);
+		items.push(PATTERN_ENDING);
 	}
 	return items;
 }
+
+/**
+ * @returns How many times pattern of specified length repeats in items sequence, starting from the end of the sequence.
+ */
+function checkPattern(items: number[], length: number): number {
+	let patternCount = 0;
+	for (let i = 0; i < length; ++i) {
+		const baseIndex = items.length - 1 - i;
+		const expectedValue = items[baseIndex];
+		let currentCount = 0;
+		for (let i = baseIndex; i >= 0; i -= length)
+			if (items[i] === expectedValue)
+				++currentCount;
+			else
+				break;
+		if (patternCount === 0 || currentCount < patternCount)
+			patternCount = currentCount;
+	}
+	return patternCount;
+}
+
+
+// ---
+
+export const fractionToDecimalEx = fractionToDecimal;
 
 if (import.meta.main) {
 	console.log(fractionToDecimal(10, 55));
--- fraction-to-recurring-decimal/main_test.ts
+++ fraction-to-recurring-decimal/main_test.ts
@@ -1,6 +1,12 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { fractionToDecimalEx } from './main.ts';
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function fractionToDecimal() {
+	assertEquals(fractionToDecimalEx(1, 2), '0.5');
+});
+Deno.test(function fractionToDecimal() {
+	assertEquals(fractionToDecimalEx(2, 1), '2');
+});
+Deno.test(function fractionToDecimal() {
+	assertEquals(fractionToDecimalEx(4, 333), '0.(012)');
 });
