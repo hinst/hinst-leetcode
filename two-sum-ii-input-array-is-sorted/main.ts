771c73d88533dd97d46f3b2ca9ffa57b4f7441a7
hinst <hinstance@yandex.ru>
2025-10-31 17:03:53 +0100

    Optimizing task: using map. Time to run got reduced from 443 milliseconds to 4 milliseconds.

--- two-sum-ii-input-array-is-sorted/main.ts
+++ two-sum-ii-input-array-is-sorted/main.ts
@@ -1,9 +1,14 @@
 function twoSum(numbers: number[], target: number): number[] {
-	for (let x = 0; x < numbers.length; ++x) {
-		for (let y = x + 1; y < numbers.length; ++y) {
-			if (numbers[x] + numbers[y] === target)
-				return [x + 1, y + 1];
-		}
+	/** value to index */
+	const map = new Map<number, number>();
+	for (let i = 0; i < numbers.length; ++i)
+		map.set(numbers[i], i);
+	for (let i = 0; i < numbers.length; ++i) {
+		const a = numbers[i];
+		const b = target - a;
+		const bIndex = map.get(b);
+		if (bIndex !== undefined)
+			return [i + 1, bIndex + 1];
 	}
 	return [0, 0];
 }
