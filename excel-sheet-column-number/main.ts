3559fc56ab734eeb7d4339d074bb6d6f2a8e45a6
hinst <hinstance@yandex.ru>
2025-11-01 13:38:01 +0100

    Completed task 171. Excel Sheet Column Number

--- excel-sheet-column-number/main.ts
+++ excel-sheet-column-number/main.ts
@@ -1,14 +1,16 @@
 const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LETTERS_MAP = new Map<string, number>();
+for (let i = 0; i < LETTERS.length; ++i)
+	LETTERS_MAP.set(LETTERS[i], i);
 
 function titleToNumber(columnTitle: string): number {
 	let result = 0;
 	let multiplier = 1;
 	for (let i = columnTitle.length - 1; i >= 0; --i) {
 		const character = columnTitle[i];
-		const index = LETTERS.indexOf(character);
+		const index = LETTERS_MAP.get(character) || 0;
 		result += (index + 1) * multiplier;
 		multiplier *= LETTERS.length;
-		console.log({character, index, result, multiplier});
 	}
 	return result;
 }
