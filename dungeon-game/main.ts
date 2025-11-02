3e8af27b144e2c1a0db376422a25284d13b3054b
hinst <hinstance@yandex.ru>
2025-11-02 12:28:32 +0100

    Find minimal health

--- dungeon-game/main.ts
+++ dungeon-game/main.ts
@@ -9,8 +9,18 @@ class Dungeon {
 	private next(row: number, column: number, currentHealth: number, minHealth: number): number {
 		const cost = this.costs[row][column];
 		currentHealth += cost;
-		minHealth = Math.min(currentHealth, minHealth);
-		console.log({row, column, currentCost: currentHealth, minHealth});
+		minHealth = Math.min(minHealth, currentHealth);
+		// console.log({row, column, currentCost: currentHealth, minHealth});
+		const isBottomAvailable = row < this.costs.length - 1;
+		const isRightAvailable = column < this.costs[row].length - 1;
+		if (!isBottomAvailable && !isRightAvailable)
+			return minHealth;
+		const nextHealth: number[] = [];
+		if (isBottomAvailable)
+			nextHealth.push(this.next(row + 1, column, currentHealth, minHealth));
+		if (isRightAvailable)
+			nextHealth.push(this.next(row, column + 1, currentHealth, minHealth));
+		minHealth = Math.min(minHealth, Math.max(...nextHealth));
 		return minHealth;
 	}
 }
