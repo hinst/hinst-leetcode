929014c5e5675c71fdd30b1eb019ca57b3d0c8dc
hinst <hinstance@yandex.ru>
2025-11-09 12:44:08 +0100

    Adding unit tests

--- dungeon-game/main_test.ts
+++ dungeon-game/main_test.ts
@@ -5,3 +5,11 @@ Deno.test(function calculateMinimumHp() {
 	const dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]];
 	assertEquals(calculateMinimumHpEx(dungeon), 7);
 });
+Deno.test(function calculateMinimumHp() {
+	const dungeon = [[0]];
+	assertEquals(calculateMinimumHpEx(dungeon), 1);
+});
+Deno.test(function calculateMinimumHp_37() {
+	const dungeon = [[3,0,-3],[-3,-2,-2],[3,1,-3]];
+	assertEquals(calculateMinimumHpEx(dungeon), 1);
+});
