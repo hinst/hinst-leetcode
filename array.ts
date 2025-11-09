0c39c79be116a1a20bd554f6beea11b6dee0db30
hinst <hinstance@yandex.ru>
2025-11-09 13:59:33 +0100

    Adding function formatItem

--- array.ts
+++ array.ts
@@ -1,5 +1,5 @@
 export function formatMatrix<T>(array: T[][]) {
-	const texts = array.map(row => row.map(item => item != null ? ('' + item) : ''));
+	const texts = array.map(row => row.map(formatItem));
 	let maxWidth = 0;
 	for (const text of texts.flatMap(item => item))
 		if (maxWidth < text.length)
@@ -9,4 +9,12 @@ export function formatMatrix<T>(array: T[][]) {
 			while (row[i].length < maxWidth)
 				row[i] = ' ' + row[i];
 	return texts.map(row => row.join(' ')).join('\n');
+}
+
+function formatItem(item: any): string {
+	if (item == null)
+		return '';
+	if (item === Number.MIN_SAFE_INTEGER)
+		return '-M';
+	return '' + item;
 }
