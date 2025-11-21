7e3c426ba9593f50d5715d133b63e3ac7c23bdbf
hinst <hinstance@yandex.ru>
2025-11-21 23:33:36 +0100

    Completed task 180. Consecutive Numbers

--- consecutive-numbers/main.sql
+++ consecutive-numbers/main.sql
@@ -1,1 +1,3 @@
-select * from logs logs1 inner join logs logs2 on (logs1.id = logs2.id + 1) and (logs1.num = logs2.num) order by logs1.id
+select distinct(logs1.num) as ConsecutiveNums from logs logs1
+  inner join logs logs2 on (logs1.id = logs2.id + 1) and (logs1.num = logs2.num)
+  inner join logs logs3 on (logs2.id = logs3.id + 1) and (logs2.num = logs3.num)
