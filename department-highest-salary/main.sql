93e397b14695396ef91a4fe064f3fce0c74598ad
hinst <hinstance@yandex.ru>
2026-01-07 22:45:58 +0100

    Completing task 184. Department Highest Salary

--- department-highest-salary/main.sql
+++ department-highest-salary/main.sql
@@ -1,3 +1,4 @@
- select * from employee employees 
-   join (select MAX(salary) as salary, departmentId from Employee group by departmentId) maxSalaries 
-   on employees.salary=maxSalaries.salary
+select Department.name as Department, employee.name as Employee, employee.salary as Salary from Employee 
+  inner join Department on Employee.departmentId=Department.id
+  inner join (select MAX(salary) as salary, departmentId from Employee group by departmentId) maxSalaries 
+  on Employee.salary=maxSalaries.salary and Employee.departmentId=maxSalaries.departmentId
