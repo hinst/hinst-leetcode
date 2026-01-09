848aee65921360414c2ae48e2f02e89516d1c7cd
hinst <hinstance@yandex.ru>
2026-01-09 21:26:57 +0100

    Preparing task 188. Best Time to Buy and Sell Stock IV

--- best-time-to-buy-and-sell-stock-iv/main.ts
+++ best-time-to-buy-and-sell-stock-iv/main.ts
@@ -1,8 +1,23 @@
+const 
+	STEP_COUNT_LIMIT = 100,
+	PRICE_COUNT_LIMIT = 1000,
+	PRICE_LIMIT = 1000;
+
 class Finder {
 	constructor(private readonly prices: number[]) {
 	}
 
 	find(stepCount: number, beginning: number, ending: number): number {
+		if (stepCount <= 0)
+			return 0;
+		const length = ending - beginning;
+		if (length <= 1)
+			return 0;
+		if (length === 2) {
+			const price1 = this.prices[beginning];
+			const price2 = this.prices[beginning + 1];
+			return price1 < price2 ? price2 - price1 : 0;
+		}
 	}
 }
 
