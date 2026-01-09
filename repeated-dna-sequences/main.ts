21bba98fff23da6a5a944886b496e234ebf0c24e
hinst <hinstance@yandex.ru>
2026-01-09 18:32:09 +0100

    Build full hash map. Passed all test cases online

--- repeated-dna-sequences/main.ts
+++ repeated-dna-sequences/main.ts
@@ -2,18 +2,18 @@ const SEQUENCE_LENGTH = 10;
 
 function findRepeatedDnaSequences(s: string): string[] {
 	const sequences = new Set<string>();
-	for (let i = 0; i < s.length - SEQUENCE_LENGTH; ++i) {
+	const duplicatedSequences = new Set<string>();
+	const threshold = s.length - SEQUENCE_LENGTH + 1;
+	for (let i = 0; i < threshold; ++i) {
 		const sequence = s.substring(i, i + SEQUENCE_LENGTH);
 		if (sequences.has(sequence))
-			continue;
-		const index = s.indexOf(sequence, i + 1);
-		if (index !== -1)
-			sequences.add(sequence);
+			duplicatedSequences.add(sequence);
+		sequences.add(sequence);
 	}
-	return Array.from(sequences);
+	return Array.from(duplicatedSequences);
 }
 
 if (import.meta.main) {
-	const s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+	const s = "AAAAAAAAAAA";
 	console.log(findRepeatedDnaSequences(s));
 }
