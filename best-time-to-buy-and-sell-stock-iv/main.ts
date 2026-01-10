d3ffc99acb5d8a5285e357955bfba565d72ff909
hinst <hinstance@yandex.ru>
2026-01-10 15:43:59 +0100

    Correcting cached behavior for find max

--- best-time-to-buy-and-sell-stock-iv/main.ts
+++ best-time-to-buy-and-sell-stock-iv/main.ts
@@ -50,11 +50,11 @@ class Finder {
 		const limit = ending - 1;
 		let bestProfit = 0;
 		for (let i = beginning; i < limit; ++i) {
-			for (let j = i + 1; j < ending; ++j) {
-				const profit = this.prices[j] - this.prices[i];
-				if (bestProfit < profit)
-					bestProfit = profit;
-			}
+			const price = this.prices[i];
+			const maxSellingPrice = this.findMaxCached(i + 1, ending);
+			const profit = maxSellingPrice - price;
+			if (bestProfit < profit)
+				bestProfit = profit;
 		}
 		return bestProfit;
 	}
