b66fee25e323516c5a2fb4bcf7438399e280b4d6
hinst <hinstance@yandex.ru>
2026-01-10 12:50:54 +0100

    Adding unit tests

--- best-time-to-buy-and-sell-stock-iv/main_test.ts
+++ best-time-to-buy-and-sell-stock-iv/main_test.ts
@@ -1,6 +1,17 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { maxProfitEx } from './main.ts';
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function maxProfit() {
+	const k = 2, prices = [2,4,1];
+	assertEquals(maxProfitEx(k, prices), 2);
+});
+
+Deno.test(function maxProfit() {
+	const k = 2, prices = [3,2,6,5,0,3]
+	assertEquals(maxProfitEx(k, prices), 7);
+});
+
+Deno.test(function maxProfit() {
+	const k = 2, prices = [1,2,4];
+	assertEquals(maxProfitEx(k, prices), 3);
 });
