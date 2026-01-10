cb8727cd6c87b4765566947ead5f1744c02d1ea8
hinst <hinstance@yandex.ru>
2026-01-10 12:51:14 +0100

    Advanced to test case 206 of 211 and got Time Limit Exceeded

--- best-time-to-buy-and-sell-stock-iv/main.ts
+++ best-time-to-buy-and-sell-stock-iv/main.ts
@@ -8,7 +8,6 @@ class Finder {
 	}
 
 	find(depth: number, stepCount: number, beginning: number, ending: number): number {
-		console.log('.'.repeat(depth), {stepCount,beginning,ending});
 		if (stepCount <= 0)
 			return 0;
 		const length = ending - beginning;
@@ -20,21 +19,24 @@ class Finder {
 			return price1 < price2 ? price2 - price1 : 0;
 		}
 		let bestProfit = 0;
-		if (stepCount === 1) {
-			for (let a = beginning; a < ending; ++a)
-				for (let b = a; b < ending; ++b) {
-					const profit = this.prices[b] - this.prices[a];
+		while (stepCount > 0) {
+			if (stepCount === 1) {
+				for (let a = beginning; a < ending; ++a)
+					for (let b = a; b < ending; ++b) {
+						const profit = this.prices[b] - this.prices[a];
+						if (bestProfit < profit)
+							bestProfit = profit;
+					}
+			}
+			for (let i = beginning + 1; i < ending; ++i) {
+				for (let step = 1; step < stepCount; ++step) {
+					const profit = this.find(depth + 1, step, beginning, i) + 
+						this.find(depth + 1, stepCount - step, i, ending);
 					if (bestProfit < profit)
 						bestProfit = profit;
 				}
-			return bestProfit;
-		}
-		for (let i = beginning + 1; i < ending; ++i) {
-			for (let step = 1; step < stepCount; ++step) {
-				const profit = this.find(depth + 1, step, beginning, i) + this.find(depth + 1, stepCount - step, i, ending);
-				if (bestProfit < profit)
-					bestProfit = profit;
 			}
+			--stepCount;
 		}
 		return bestProfit;
 	}
@@ -45,7 +47,10 @@ function maxProfit(k: number, prices: number[]): number {
 	return finder.find(0, k, 0, prices.length);
 }
 
+
+export const maxProfitEx = maxProfit;
+
 if (import.meta.main) {
-	const k = 2, prices = [3,2,6,5,0,3];
+	const k = 2, prices = [1,2,4];
 	console.log(maxProfit(k, prices));
 }
