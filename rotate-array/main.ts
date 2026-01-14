43474b7371a1cb30bb07c970a80a82e686de862d
hinst <hinstance@yandex.ru>
2026-01-14 22:59:21 +0100

    Completed task 189. Rotate Array

--- rotate-array/main.ts
+++ rotate-array/main.ts
@@ -1,9 +1,16 @@
 /**
 	Do not return anything, modify nums in-place instead.
 */
-function rotate(nums: number[], k: number): void {
-
-};
+function rotate(items: number[], steps: number): void {
+	const source = items.slice(0);
+	for (let i = 0; i < items.length; ++i) {
+		const newIndex = (i + steps) % items.length;
+		items[newIndex] = source[i];
+	}
+}
 
 if (import.meta.main) {
+	const nums = [1,2,3,4,5,6,7], k = 3;
+	rotate(nums, k);
+	console.log(nums.join(', '));
 }
