1e4b2549ad8c8a4c68b2c7a1c4919ef2a76effb2
hinst <hinstance@yandex.ru>
2026-01-18 15:03:51 +0100

    Completed task 194. Transpose File

--- transpose-file/main.bash
+++ transpose-file/main.bash
@@ -1,3 +1,21 @@
+# Read from the file file.txt and print its transposed content to stdout.
+declare -a rows
 while read -r line || [ -n "$line" ]
 do
+	wordIndex=0
+	for word in $line
+	do
+		if [ -z "${rows[$wordIndex]}" ]
+		then
+			rows[$wordIndex]="$word"
+		else
+			rows[$wordIndex]="${rows[$wordIndex]} $word"
+		fi
+		wordIndex=$(( wordIndex + 1 ))
+	done
 done < "file.txt"
+
+for rowIndex in ${!rows[@]}
+do
+	echo "${rows[$rowIndex]}"
+done
