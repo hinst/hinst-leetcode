2d5be723b1f31574d9b0dc3857829703159f4979
hinst <hinstance@yandex.ru>
2026-01-18 14:17:20 +0100

    Completed task 193. Valid Phone Numbers

--- valid-phone-numbers/main.bash
+++ valid-phone-numbers/main.bash
@@ -1,7 +1,8 @@
+# Read from the file file.txt and output all valid phone numbers to stdout.
 while read -r line || [ -n "$line" ]
 do
 	# (xxx) xxx-xxxx or xxx-xxx-xxxx
-	if [[ "$line" =~ ^\(\d\d\d\)\ \d\d\d-\d\d\d\d$ ]] || [[ "$line" =~ ^\d\d\d-\d\d\d-\d\d\d\d$ ]]
+	if [[ "$line" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]] || [[ "$line" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]
 	then
 		echo "$line"
 	fi
