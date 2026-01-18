ccccdb14e1ca79f247c90ded407d190d1393497f
hinst <hinstance@yandex.ru>
2026-01-18 17:16:46 +0100

    Adding class Robber

--- house-robber/main.ts
+++ house-robber/main.ts
@@ -1,6 +1,21 @@
-function rob(nums: number[]): number {
+class Robber {
+	constructor(private readonly treasures: number[]) {
+	}
 
+	find(index: number): number {
+		if (index >= this.treasures.length)
+			return 0;
+		return Math.max(
+			this.find(index + 1),
+			this.treasures[index] + this.find(index + 2)
+		);
+	}
 }
+
+function rob(treasures: number[]): number {
+	return new Robber(treasures).find(0);
+}
+
 
 if (import.meta.main) {
 	console.log(rob([1,2,3,1]));
