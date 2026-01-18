efb79a1e3a0e8d31344819a971b8b0a27b91321d
hinst <hinstance@yandex.ru>
2026-01-18 19:05:42 +0100

    Completed task 199. Binary Tree Right Side View

--- binary-tree-right-side-view/main.ts
+++ binary-tree-right-side-view/main.ts
@@ -1,8 +1,22 @@
 import { TreeNode} from '../treeNode.ts';
 
 function rightSideView(root: TreeNode | null): number[] {
-
+	const lastColumn: number[] = [];
+	advance(lastColumn, 0, root);
+	return lastColumn;
 }
 
+function advance(lastColumn: number[], depth: number, node: TreeNode | null) {
+	if (!node)
+		return;
+	while (lastColumn[depth] == null)
+		lastColumn.push(0);
+	lastColumn[depth] = node.val;
+	advance(lastColumn, depth + 1, node.left);
+	advance(lastColumn, depth + 1, node.right);
+}
+
+
 if (import.meta.main) {
+	console.log(rightSideView(TreeNode.unwrap([1,2,3,null,5,null,4])));
 }
