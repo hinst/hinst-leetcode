09b31c2e8b24e26593a5281c1fd231db8ac3eca3
hinst <hinstance@yandex.ru>
2026-01-18 13:46:28 +0100

    Added regular expression matchers, but it does not seem to work

--- valid-phone-numbers/main.bash
+++ valid-phone-numbers/main.bash
@@ -1,3 +1,8 @@
 while read -r line || [ -n "$line" ]
 do
+	# (xxx) xxx-xxxx or xxx-xxx-xxxx
+	if [[ "$line" =~ ^\(\d\d\d\)\ \d\d\d-\d\d\d\d$ ]] || [[ "$line" =~ ^\d\d\d-\d\d\d-\d\d\d\d$ ]]
+	then
+		echo "$line"
+	fi
 done < "file.txt"
