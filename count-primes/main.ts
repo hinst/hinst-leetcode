ea5f66a4d656096ea0ec0ff82471254304989882
hinst <hinstance@yandex.ru>
2026-03-28 12:51:27 +0100

    Using Sieve of Eratosthenes to generate prime number table, as suggested in the hint from the task description

--- count-primes/main.ts
+++ count-primes/main.ts
@@ -1,76 +1,55 @@
-const primes: number[] = [2];
+const LIMIT = 5 * 10**6;
+const primes: boolean[] = new Array(LIMIT).fill(true);
 
-function findPrimeIndex(start: number, end: number, targetValue: number): number | undefined {
-	if (end <= start)
-		return undefined;
-	const middleIndex = Math.trunc((start + end) / 2);
-	const middleValue = primes[middleIndex];
-	if (middleValue === targetValue)
-		return middleIndex - 1;
-	if (middleValue < targetValue && middleIndex < primes.length - 1 && targetValue <= primes[middleIndex + 1])
-		return middleIndex;
-	const left = findPrimeIndex(start, middleIndex, targetValue);
-	if (left !== undefined)
-		return left;
-	const right = findPrimeIndex(middleIndex + 1, end, targetValue);
-	return right;
+function generate() {
+	primes[0] = false;
+	primes[1] = false;
+	let prime = 2;
+	do {
+		for (let i = 2; i < LIMIT; ++i) {
+			const composite = prime * i;
+			if (composite >= LIMIT)
+				break;
+			primes[composite] = false;
+		}
+		let nextPrime = 0;
+		for (let i = prime + 1; i < LIMIT; ++i)
+			if (primes[i]) {
+				nextPrime = i;
+				break;
+			}
+		prime = nextPrime;
+	} while (prime);
 }
 
 function countPrimes(n: number): number {
 	if (n <= 2)
 		return 0;
-	const primeIndex = findPrimeIndex(0, primes.length, n);
-	if (primeIndex !== undefined)
-		return primeIndex + 1;
-	// console.log(primes[primes.length - 1]);
-	for (let counter = primes[primes.length - 1] + 1; counter < n; ++counter) {
-		let isPrime = true;
-		const half = counter / 2;
-		for (const prime of primes) {
-			if (counter % prime === 0) {
-				isPrime = false;
-				break;
-			}
-			if (half < prime)
-				break;
-		}
-		if (isPrime)
-			primes.push(counter);
-	}
-	return primes.length;
+	return 0;
 }
 
-
-import { writeFileSync } from 'node:fs';
-
-function generate() {
-	const LIMIT = 5 * 10**6;
-	const primes: number[] = [2];
-	for (let i = 3; i <= LIMIT; ++i) {
-		let isPrime = true;
-		for (const prime of primes)
-			if (i % prime === 0) {
-				isPrime = false;
-				break;
-			}
-		if (isPrime)
-			primes.push(i);
-	}
-	console.log('primes.length', primes.length);
-	writeFileSync('./primes.json', JSON.stringify(primes));
-}
 
 export const countPrimesExported = countPrimes;
 
 if (import.meta.main) {
-	console.time('generate');
-	generate();
-	console.timeEnd('generate');
-
+	if (true) {
+		console.time('generate');
+		generate();
+		if (false) {
+			let count = 0;
+			for (let i = 0; i < primes.length; ++i) {
+				if (primes[i]) {
+					console.log(i);
+					++count;
+				}
+			}
+			console.log('[', count, ']');
+		}
+		console.timeEnd('generate');
+	}
 	if (false) {
 		console.time('countPrimes');
-		console.log(countPrimes(8));
-		console.log(countPrimes(3));
+		console.log(countPrimes(LIMIT));
 		console.timeEnd('countPrimes');
 	}
 }
