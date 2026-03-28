2bb069da2562b1be087784334de1d530083aac63
hinst <hinstance@yandex.ru>
2026-03-28 12:12:58 +0100

    Adding binary search to find prime index. Adding function generate() to save all prime numbers to a file

--- count-primes/main.ts
+++ count-primes/main.ts
@@ -1,14 +1,28 @@
 const primes: number[] = [2];
 
+function findPrimeIndex(start: number, end: number, targetValue: number): number | undefined {
+	if (end <= start)
+		return undefined;
+	const middleIndex = Math.trunc((start + end) / 2);
+	const middleValue = primes[middleIndex];
+	if (middleValue === targetValue)
+		return middleIndex - 1;
+	if (middleValue < targetValue && middleIndex < primes.length - 1 && targetValue <= primes[middleIndex + 1])
+		return middleIndex;
+	const left = findPrimeIndex(start, middleIndex, targetValue);
+	if (left !== undefined)
+		return left;
+	const right = findPrimeIndex(middleIndex + 1, end, targetValue);
+	return right;
+}
+
 function countPrimes(n: number): number {
 	if (n <= 2)
 		return 0;
-	for (let i = 0; i < primes.length; ++i) {
-		if (n <= primes[i]) {
-			return i;
-		}
-	}
-	console.log(primes[primes.length - 1]);
+	const primeIndex = findPrimeIndex(0, primes.length, n);
+	if (primeIndex !== undefined)
+		return primeIndex + 1;
+	// console.log(primes[primes.length - 1]);
 	for (let counter = primes[primes.length - 1] + 1; counter < n; ++counter) {
 		let isPrime = true;
 		const half = counter / 2;
@@ -26,11 +40,37 @@ function countPrimes(n: number): number {
 	return primes.length;
 }
 
+
+import { writeFileSync } from 'node:fs';
+
+function generate() {
+	const LIMIT = 5 * 10**6;
+	const primes: number[] = [2];
+	for (let i = 3; i <= LIMIT; ++i) {
+		let isPrime = true;
+		for (const prime of primes)
+			if (i % prime === 0) {
+				isPrime = false;
+				break;
+			}
+		if (isPrime)
+			primes.push(i);
+	}
+	console.log('primes.length', primes.length);
+	writeFileSync('./primes.json', JSON.stringify(primes));
+}
+
 export const countPrimesExported = countPrimes;
 
 if (import.meta.main) {
-	console.time('countPrimes');
-	console.log(countPrimes(8));
-	console.log(countPrimes(12));
-	console.timeEnd('countPrimes');
+	console.time('generate');
+	generate();
+	console.timeEnd('generate');
+
+	if (false) {
+		console.time('countPrimes');
+		console.log(countPrimes(8));
+		console.log(countPrimes(3));
+		console.timeEnd('countPrimes');
+	}
 }
