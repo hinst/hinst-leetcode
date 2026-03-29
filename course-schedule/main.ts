f49d3218692419609660a1e779565e47d1bc3212
hinst <hinstance@yandex.ru>
2026-03-29 18:06:30 +0200

    Reached online test case 48 of 54 and got Time Limit Exceeded

--- course-schedule/main.ts
+++ course-schedule/main.ts
@@ -1,14 +1,15 @@
 function hasLoop(id: number, visitedNodes: Set<number>, map: Map<number, number[]>): boolean {
 	if (visitedNodes.has(id))
 		return true;
-	visitedNodes.add(id);
 	const sources = map.get(id);
 	if (!sources)
 		return false;
+	visitedNodes.add(id);
 	for (const source of sources) {
 		if (hasLoop(source, visitedNodes, map))
 			return true;
 	}
+	visitedNodes.delete(id);
 	return false;
 }
 
@@ -35,8 +36,6 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
 export const canFinishPublic = canFinish;
 
 if (import.meta.main) {
-	let numCourses = 2, prerequisites = [[1,0]];
-	console.log(canFinish(numCourses, prerequisites));
-	numCourses = 2, prerequisites = [[1,0],[0,1]];
+	const numCourses = 5, prerequisites = [[1,4],[2,4],[3,1],[3,2]];
 	console.log(canFinish(numCourses, prerequisites));
 }
--- course-schedule/main_test.ts
+++ course-schedule/main_test.ts
@@ -10,3 +10,8 @@ Deno.test(function canFinish() {
 	const numCourses = 2, prerequisites = [[1,0],[0,1]];
 	assertEquals(canFinishPublic(numCourses, prerequisites), false);
 });
+
+Deno.test(function canFinish() {
+	const numCourses = 5, prerequisites = [[1,4],[2,4],[3,1],[3,2]];
+	assertEquals(canFinishPublic(numCourses, prerequisites), true);
+});
